f9f823ca55d0d461ab003e000ae0599726834f67
metaclips <utimichael9@gmail.com>
2020-01-15 23:32:44 +0100

    netType is a const

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"github.com/decred/dcrd/dcrutil"
 )
 
-const appDisplayName = "GoDCR"
+const (
+	appDisplayName = "GoDCR"
+	netType        = "testnet3"
+)
 
 var defaultAppDataDir = dcrutil.AppDataDir("godcr", false)
 
 func main() {
-	launchUserInterface(appDisplayName, defaultAppDataDir, "testnet3")
+	launchUserInterface(appDisplayName, defaultAppDataDir, netType)
 }
