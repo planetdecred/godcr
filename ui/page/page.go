bfc8a2b4197ba4c5fd0789b543148963d3b15ecc
Oluwandabira Alawode <dabiraalawode@yahoo.com>
2020-01-17 13:51:10 +0200

    Fix actions

--- ui/page/page.go
+++ ui/page/page.go
@@ -1,5 +1,5 @@
 // Package page provides an interface and implementations
-// for creating and using pages
+// for creating and using pages.
 package page
 
 import (
@@ -7,11 +7,11 @@ import (
 	"gioui.org/widget/material"
 )
 
-// Page represents a single page of the app
+// Page represents a single page of the app.
 //
 // Init creates widgets with the given theme and
-// layout context
-// Implementations must store the layout context
+// layout context.
+// Implementations must store the layout context.
 //
 // Draw adds the implementation's widgets to the stored
 // layout context
