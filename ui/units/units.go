5597b6464a8713c2096344b32772fea4a9cc6f57
Oluwandabira Alawode <dabiraalawode@yahoo.com>
2020-01-17 13:21:37 +0200

    Add initital  dummy landing page

--- ui/units/units.go
+++ ui/units/units.go
@@ -1,14 +1,9 @@
-// Package units provides unit values used across the app.
+// Package units provides unit values used across the app
 package units
 
 import (
 	"gioui.org/unit"
 )
 
-var (
-	// Label is the unit for the app labels
-	Label = unit.Dp(50)
-
-	// FlexInset is the unit for flex insets
-	FlexInset = unit.Dp(50)
-)
+// Label is the unit for the app labels
+var Label = unit.Dp(50)
