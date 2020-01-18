cc7d22ef56107a0fc3620f8e90dea24e38af1e19
Oluwandabira Alawode <dabiraalawode@yahoo.com>
2020-01-19 00:23:59 +0200

    Correct tests

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -2,14 +2,25 @@ package wallet_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
+	"github.com/raedahgroup/godcr-gio/event"
 	. "github.com/raedahgroup/godcr-gio/wallet"
 )
 
-var _ = Describe("Sync", func() {
-	It(`works with a zero wallet`, func() {
-		wal := new(Wallet)
-		wal.Sync()
-		// just checking that this doesn't panic
+var _ = Describe("Wallet", func() {
+	Context(`works with an almost zero wallet`, func() {
+		It(`passes an error to SendChan when Sync is called`, func() {
+			send := make(chan event.Event)
+			wal := &Wallet{
+				SendChan: send,
+			}
+			go wal.Sync()
+
+			// TODO: check that it sends the correct error
+			e := <-send
+			_, ok := e.(error)
+			Expect(ok).To(Equal(true))
+
+		})
 	})
 })
