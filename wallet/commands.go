058d0d38d663998642713aaa5a70516746aa8293
Alawode Oluwandabira <24304808+RogueElement@users.noreply.github.com>
2020-01-26 12:23:42 +0200

    Added wallet creation functionality to the back end (#14)
    Added back end functionality to:
    
    - Create a new wallet.
    - Restore a wallet.
    - Start and complete a SPV sync.
    - Get basic wallet/network info.

---
+++ wallet/commands.go
@@ -0,0 +1,98 @@
+package wallet
+
+import (
+	"errors"
+
+	"github.com/raedahgroup/godcr-gio/event"
+)
+
+var (
+	// ErrInvalidArguments is returned when a wallet command is send with invalid arguments.
+	ErrInvalidArguments = errors.New("invalid command arguments")
+
+	// ErrNotFound is returned when a wallet command is given that does not exist or is not
+	// implemented.
+	ErrNotFound = errors.New("command not found or not implemented")
+)
+
+var cmdMap = map[string]func(*Wallet, *event.ArgumentQueue) error{
+	event.CreateCmd:  createCmd,
+	event.RestoreCmd: restoreCmd,
+	event.InfoCmd:    infoCmd,
+}
+
+func createCmd(wal *Wallet, arguments *event.ArgumentQueue) error {
+	passphrase, err := arguments.PopString()
+	if err != nil {
+		return ErrInvalidArguments
+	}
+	passtype, err := arguments.PopInt()
+	if err != nil {
+		return ErrInvalidArguments
+	}
+
+	wall, err := wal.multi.CreateNewWallet(passphrase, int32(passtype))
+	if err != nil {
+		return err
+	}
+	wal.Send <- event.WalletResponse{
+		Resp: event.CreatedResp,
+		Results: &event.ArgumentQueue{
+			Queue: []interface{}{wall.Seed},
+		},
+	}
+	return nil
+}
+
+func restoreCmd(wal *Wallet, arguments *event.ArgumentQueue) error {
+	seed, err := arguments.PopString()
+	if err != nil {
+		return ErrInvalidArguments
+	}
+
+	passphrase, err := arguments.PopString()
+	if err != nil {
+		return ErrInvalidArguments
+	}
+	passtype, err := arguments.PopInt()
+	if err != nil {
+		return ErrInvalidArguments
+	}
+
+	_, err = wal.multi.RestoreWallet(seed, passphrase, int32(passtype))
+	if err != nil {
+		return err
+	}
+
+	wal.Send <- event.WalletResponse{
+		Resp: event.RestoredResp,
+	}
+
+	return nil
+}
+
+func infoCmd(wal *Wallet, _ *event.ArgumentQueue) error {
+	wallets, err := wal.wallets()
+	if err != nil {
+		return err
+	}
+	var completeTotal int64
+	for _, wall := range wallets {
+		iter, err := wall.AccountsIterator(2) // Placeholder
+		if err != nil {
+			return err
+		}
+		for acct := iter.Next(); acct != nil; acct = iter.Next() {
+			completeTotal += acct.TotalBalance
+		}
+	}
+	best := wal.multi.GetBestBlock()
+
+	wal.Send <- event.WalletInfo{
+		LoadedWallets:   len(wallets),
+		TotalBalance:    completeTotal,
+		BestBlockHeight: best.Height,
+		BestBlockTime:   best.Timestamp,
+	}
+	return nil
+}
--- wallet/sync.go
+++ wallet/sync.go
@@ -2,32 +2,106 @@ package wallet
 
 import (
 	"fmt"
+	"sync"
 
+	"github.com/raedahgroup/dcrlibwallet"
 	"github.com/raedahgroup/godcr-gio/event"
 )
 
+const syncID = "godcr"
+
 // Sync is the main wallet sync loop
-func (wal *Wallet) Sync() {
-	loaded, err := wal.loadWallets()
+func (wal *Wallet) Sync(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	err := wal.loadWallets(wal.root, wal.net)
 	if err != nil {
 		wal.Send <- err
 		return
 	}
-	defer wal.multi.Shutdown()
 
-	wal.Send <- event.Loaded{
-		WalletsLoadedCount: loaded,
+	//fmt.Println("Sending loaded event")
+	wal.Send <- event.WalletResponse{
+		Resp: event.LoadedWalletsResp,
+		Results: &event.ArgumentQueue{
+			Queue: []interface{}{int(wal.multi.LoadedWalletsCount())},
+		},
 	}
 
+	defer wal.multi.Shutdown()
+
+	err = wal.multi.AddSyncProgressListener(&progressListener{
+		Send: wal.Send,
+	}, syncID)
+	if err != nil {
+		wal.Send <- err
+		return
+	}
 	for {
 		e := <-wal.Receive
 		if cmd, ok := e.(event.WalletCmd); ok {
 			switch cmd.Cmd {
+			case event.StartSyncCmd:
+				if !wal.multi.IsSyncing() {
+					go func(c chan<- event.Event) {
+						err := wal.multi.SpvSync()
+						if err != nil {
+							c <- err
+						}
+					}(wal.Send)
+				}
 			case event.ShutdownCmd:
 				return
+			default:
+				if fun, ok := cmdMap[cmd.Cmd]; ok {
+					err := fun(wal, cmd.Arguments)
+					if err != nil {
+						wal.Send <- err
+					}
+				}
 			}
 		} else {
 			fmt.Printf("Not a wallet command %+v\n", e)
 		}
 	}
+}
+
+type progressListener struct {
+	Send chan<- event.Event
+}
+
+func (listener *progressListener) Debug(info *dcrlibwallet.DebugInfo) {
+
+}
+
+func (listener *progressListener) OnSyncStarted() {
+	listener.Send <- event.Sync{Event: event.SyncStart}
+}
+
+func (listener *progressListener) OnPeerConnectedOrDisconnected(numberOfConnectedPeers int32) {
+
+}
+
+func (listener *progressListener) OnHeadersFetchProgress(headersFetchProgress *dcrlibwallet.HeadersFetchProgressReport) {
+
+}
+
+func (listener *progressListener) OnAddressDiscoveryProgress(addressDiscoveryProgress *dcrlibwallet.AddressDiscoveryProgressReport) {
+
+}
+
+func (listener *progressListener) OnHeadersRescanProgress(headersRescanProgress *dcrlibwallet.HeadersRescanProgressReport) {
+
+}
+
+func (listener *progressListener) OnSyncCompleted() {
+	listener.Send <- event.Sync{Event: event.SyncEnd}
+}
+
+func (listener *progressListener) OnSyncCanceled(willRestart bool) {
+
+}
+
+func (listener *progressListener) OnSyncEndedWithError(err error) {
+
 }
