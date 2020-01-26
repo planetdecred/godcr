b526393457d2dfc83ad3adb8ff368492dba5ad38
metaclips <utimichael9@gmail.com>
2020-01-26 17:17:11 +0100

    implement create/restore landing page

---
+++ ui/page/createwallet.go
@@ -0,0 +1,134 @@
+package page
+
+import (
+	"fmt"
+	"image/color"
+	"image/png"
+	"log"
+	"os"
+
+	"gioui.org/layout"
+	"gioui.org/op/paint"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+	"github.com/raedahgroup/godcr-gio/ui/widgets"
+
+	"github.com/raedahgroup/godcr-gio/event"
+)
+
+// LandingID is the id of the landing page.
+const Create = "Create"
+
+// Landing represents the landing page of the app.
+// It should only be should shown if the app launches
+// and cannot find any wallets.
+type CreateAndRestore struct {
+	container    *layout.List
+	image        material.Image
+	welcomeLabel material.Label
+
+	createButton  *widget.Button
+	restoreButton *widget.Button
+
+	addIcon     material.Image
+	restoreIcon material.Image
+}
+
+// Init adds a heading and two buttons.
+func (pg *CreateAndRestore) Init(theme *material.Theme) {
+	file, err := os.Open("./assets/decred.png")
+	fmt.Println(os.UserHomeDir())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	image, err := png.Decode(file)
+	pg.image = theme.Image(paint.NewImageOp(image))
+
+	pg.welcomeLabel = theme.Label(unit.Sp(24), "Welcome to\nDecred desktop wallet")
+
+	pg.createButton = new(widget.Button)
+	pg.restoreButton = new(widget.Button)
+
+	file, err = os.Open("./assets/add.png")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	image, err = png.Decode(file)
+	pg.addIcon = theme.Image(paint.NewImageOp(image))
+
+	file, err = os.Open("./assets/restore.png")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	image, err = png.Decode(file)
+	pg.restoreIcon = theme.Image(paint.NewImageOp(image))
+
+	pg.container = &layout.List{
+		Axis: layout.Vertical,
+	}
+}
+
+// Draw draws the page's to the given layout context.
+// Does not react to any event but can return a Nav event.
+func (pg *CreateAndRestore) Draw(gtx *layout.Context, _ event.Event) (evt event.Event) {
+	widgets := []func(){
+		func() {
+			//bb.Layout(gtx, unit.Dp(32))
+			//	th.IconButton(bb).Layout(gtx, button)
+			gtx.Dimensions.Size.Y = 24
+		},
+
+		func() {
+			pg.image.Scale = 0.5
+			pg.image.Layout(gtx)
+		},
+
+		func() {
+			gtx.Dimensions.Size.Y = 24
+		},
+
+		func() {
+			pg.welcomeLabel.Layout(gtx)
+		},
+
+		func() {
+			gtx.Dimensions.Size.Y = 550
+		},
+
+		func() {
+			gtx.Constraints.Width.Min = gtx.Px(unit.Dp(312))
+			gtx.Constraints.Width.Max = gtx.Px(unit.Dp(312))
+			gtx.Constraints.Height.Min = gtx.Px(unit.Dp(56))
+			gtx.Constraints.Height.Max = gtx.Px(unit.Dp(56))
+
+			imgBt := widgets.NewImageButton(&pg.addIcon, "Create a new wallet")
+			imgBt.Axis = layout.Horizontal
+			imgBt.Background = color.RGBA{41, 112, 255, 255}
+			imgBt.Padding = unit.Dp(20)
+
+			imgBt.Layout(gtx, pg.createButton, 20)
+		},
+
+		func() {
+			gtx.Constraints.Width.Min = gtx.Px(unit.Dp(312))
+			gtx.Constraints.Width.Max = gtx.Px(unit.Dp(312))
+			gtx.Constraints.Height.Min = gtx.Px(unit.Dp(56))
+			gtx.Constraints.Height.Max = gtx.Px(unit.Dp(56))
+
+			//fmt.Println(img)
+			imgBt := widgets.NewImageButton(&pg.restoreIcon, "Restore an existing wallet")
+			imgBt.Axis = layout.Horizontal
+			imgBt.Background = color.RGBA{45, 216, 163, 255}
+			imgBt.Padding = unit.Dp(20)
+
+			imgBt.Layout(gtx, pg.restoreButton, 20)
+		},
+	}
+
+	pg.container.Layout(gtx, len(widgets), func(i int) {
+		layout.Inset{Top: unit.Dp(8), Left: unit.Dp(24), Right: unit.Dp(24), Bottom: unit.Dp(8)}.Layout(gtx, widgets[i])
+	})
+
+	return
+}
---
+++ ui/widgets/imagebutton.go
@@ -0,0 +1,153 @@
+package widgets
+
+import (
+	"image"
+	"image/color"
+
+	"gioui.org/f32"
+	"gioui.org/font"
+	"gioui.org/io/pointer"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+	gioText "gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+type ImageButton struct {
+	Text         string
+	Font         gioText.Font
+	Color        color.RGBA
+	Background   color.RGBA
+	Size         unit.Value
+	Padding      unit.Value
+	CornerRadius unit.Value
+	shaper       gioText.Shaper
+
+	alignment layout.Alignment
+	Axis      layout.Axis
+	Src       *material.Image
+}
+
+func NewImageButton(img *material.Image, text string) ImageButton {
+	return ImageButton{
+		Text: text,
+		Font: gioText.Font{
+			Size: unit.Sp(16).Scale(14.0 / 16.0),
+		},
+		Color:      rgb(0xffffff),
+		Background: rgb(0x3f51b5),
+		shaper:     font.Default(),
+		alignment:  layout.Middle,
+		Axis:       layout.Horizontal,
+
+		Src:     img,
+		Size:    unit.Dp(56),
+		Padding: unit.Dp(16),
+	}
+}
+
+func (b ImageButton) Layout(gtx *layout.Context, button *widget.Button, buttonTextSpace float32) {
+	layout.Stack{}.Layout(gtx,
+		layout.Expanded(func() {
+			rr := float32(gtx.Px(unit.Dp(8)))
+			clip.Rect{
+				Rect: f32.Rectangle{Max: f32.Point{
+					X: float32(gtx.Constraints.Width.Min),
+					Y: float32(gtx.Constraints.Height.Min),
+				}},
+				NE: rr, NW: rr, SE: rr, SW: rr,
+			}.Op(gtx.Ops).Add(gtx.Ops)
+
+			fill(gtx, b.Background)
+			for _, c := range button.History() {
+				drawInk(gtx, c)
+			}
+		}),
+
+		layout.Stacked(func() {
+			iconAndLabel := layout.Flex{Axis: b.Axis, Alignment: layout.Middle}
+
+			icon := layout.Rigid(func() {
+				layout.Inset{Top: b.Padding, Left: b.Padding, Bottom: b.Padding, Right: unit.Dp(buttonTextSpace / 2)}.Layout(gtx, func() {
+					b.Src.Scale = 0.2
+					b.Src.Layout(gtx)
+				})
+			})
+
+			label := layout.Rigid(func() {
+				layout.Inset{Top: b.Padding, Right: b.Padding, Bottom: b.Padding, Left: unit.Dp(buttonTextSpace / 2)}.Layout(gtx, func() {
+					paint.ColorOp{Color: b.Color}.Add(gtx.Ops)
+					widget.Label{Alignment: gioText.Start}.Layout(gtx, b.shaper, b.Font, b.Text)
+				})
+
+			})
+
+			if b.Src != nil {
+				iconAndLabel.Layout(gtx, icon, label)
+			} else {
+				iconAndLabel.Layout(gtx, label)
+			}
+
+			pointer.Rect(image.Rectangle{Max: gtx.Dimensions.Size}).Add(gtx.Ops)
+			button.Layout(gtx)
+		}),
+	)
+}
+
+func drawInk(gtx *layout.Context, c widget.Click) {
+	d := gtx.Now().Sub(c.Time)
+	t := float32(d.Seconds())
+	const duration = 0.5
+	if t > duration {
+		return
+	}
+
+	t = t / duration
+	var stack op.StackOp
+	stack.Push(gtx.Ops)
+	size := float32(gtx.Px(unit.Dp(700))) * t
+	rr := size * .5
+	col := byte(0xaa * (1 - t*t))
+	ink := paint.ColorOp{Color: color.RGBA{A: col, R: col, G: col, B: col}}
+	ink.Add(gtx.Ops)
+
+	op.TransformOp{}.Offset(c.Position).Offset(f32.Point{
+		X: -rr,
+		Y: -rr,
+	}).Add(gtx.Ops)
+
+	clip.Rect{
+		Rect: f32.Rectangle{Max: f32.Point{
+			X: float32(size),
+			Y: float32(size),
+		}},
+		NE: rr, NW: rr, SE: rr, SW: rr,
+	}.Op(gtx.Ops).Add(gtx.Ops)
+
+	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: float32(size), Y: float32(size)}}}.Add(gtx.Ops)
+	stack.Pop()
+	op.InvalidateOp{}.Add(gtx.Ops)
+}
+
+func fill(gtx *layout.Context, col color.RGBA) {
+	cs := gtx.Constraints
+	d := image.Point{X: cs.Width.Min, Y: cs.Height.Min}
+	dr := f32.Rectangle{
+		Max: f32.Point{X: float32(d.X), Y: float32(d.Y)},
+	}
+	paint.ColorOp{Color: col}.Add(gtx.Ops)
+	paint.PaintOp{Rect: dr}.Add(gtx.Ops)
+	gtx.Dimensions = layout.Dimensions{Size: d}
+}
+
+func rgb(c uint32) color.RGBA {
+	return argb(0xff000000 | c)
+}
+
+func argb(c uint32) color.RGBA {
+	return color.RGBA{A: uint8(c >> 24), R: uint8(c >> 16), G: uint8(c >> 8), B: uint8(c)}
+}
