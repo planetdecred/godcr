b8d29fbf5fd445ba4e38b5af2ce8320ecddce8ee
Alawode Oluwandabira <24304808+RogueElement@users.noreply.github.com>
2020-02-02 20:21:54 +0200

    Change states to use a map

--- ui/page/page.go
+++ ui/page/page.go
@@ -12,16 +12,17 @@ import (
 // Page represents a single page of the app.
 //
 // Init creates widgets with the given theme.
-// Implementations can store the event Duplex for
-// communication with the window
 //
 // Draw draws the implementation's widgets to the given
 // layout context with regards to the given states.
-// The given states must have a *wallet.MultiWalletInfo as the first
-// element.
 // Draw returns any window event not handled by page itself.
 // Draw is only called once per frame for the active page.
 type Page interface {
-	Init(*materialplus.Theme, *wallet.Wallet)
-	Draw(gtx *layout.Context, states ...interface{}) interface{}
+	Init(*materialplus.Theme, *wallet.Wallet, map[string]interface{})
+	Draw(gtx *layout.Context) interface{}
 }
+
+const (
+	// StateWalletInfo is the map key for the WalletInfo state
+	StateWalletInfo = "walletinfo"
+)
