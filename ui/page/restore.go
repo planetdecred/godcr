c28576cd92f84e55024dca6f38bfd9ab08798202
pvsong <song@zz.local>
2020-02-05 22:56:29 +0700

    Validate inputs when button click

--- ui/page/restore.go
+++ ui/page/restore.go
@@ -1,7 +1,6 @@
 package page
 
 import (
-	"fmt"
 	"strconv"
 
 	"gioui.org/layout"
@@ -29,6 +28,9 @@ type Restore struct {
 	restoreBtn material.Button
 	restoreWdg *widget.Button
 
+	backBtn material.Button
+	backWdg *widget.Button
+
 	inputGroupContainer layout.List
 	inputGroupHeader    material.Label
 	inputLabels         []material.Label
@@ -50,6 +52,7 @@ type modal struct {
 	cancelWdg             *widget.Button
 	submitBtn             material.Button
 	submitWdg             *widget.Button
+	confirmErrorMsg       material.Label
 }
 
 // Init initializes restore page with heading, 33 inputs and button
@@ -65,14 +68,18 @@ func (pg *Restore) Init(theme *materialplus.Theme, _ *wallet.Wallet, states map[
 	pg.inputGroupContainer.Axis = layout.Vertical
 
 	pg.restoreBtn = theme.Button("Restore")
-	pg.restoreBtn.Background = LightGrayColor
+	pg.restoreBtn.Background = LightBlueColor
 	pg.restoreWdg = new(widget.Button)
+
+	pg.backBtn = theme.Button("Back")
+	pg.backBtn.Background = LightGrayColor
+	pg.backWdg = new(widget.Button)
 
 	for i := 0; i <= 32; i++ {
 		pg.inputs = append(pg.inputs, theme.Editor("Input word "+strconv.Itoa(i+1)+"..."))
 		pg.inputs[i].Font.Size = unit.Sp(16)
 		pg.editors = append(pg.editors, &widget.Editor{SingleLine: true})
-		pg.inputLabels = append(pg.inputLabels, theme.Label(unit.Dp(13), "Word#"+strconv.Itoa(i+1)))
+		pg.inputLabels = append(pg.inputLabels, theme.Label(unit.Dp(13), "Word #"+strconv.Itoa(i+1)))
 	}
 
 	pg.showModal = false
@@ -82,6 +89,9 @@ func (pg *Restore) Init(theme *materialplus.Theme, _ *wallet.Wallet, states map[
 // initModal initializes modal with inputs password, confirm password, PIN
 // and buttons
 func (pg *modal) initModal(theme *materialplus.Theme) {
+	pg.confirmErrorMsg = theme.Label(unit.Dp(16), "")
+	pg.confirmErrorMsg.Color = DangerColor
+
 	pg.inputPassword = theme.Editor("Password...")
 	pg.editorPassword = &widget.Editor{SingleLine: true}
 	pg.inputConfirmPassword = theme.Editor("Confirm password...")
@@ -127,31 +137,58 @@ func (pg *Restore) Draw(gtx *layout.Context) interface{} {
 				)
 			}),
 			layout.Rigid(func() {
-				gtx.Constraints.Height.Min = 44
 				gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
-				pg.restoreBtn.Layout(gtx, pg.restoreWdg)
+				layout.Inset{Top: unit.Dp(10)}.Layout(gtx, func() {
+					layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+						layout.Rigid(func() {
+							pg.backBtn.Layout(gtx, pg.backWdg)
+						}),
+						layout.Rigid(func() {
+							layout.Inset{Left: unit.Dp(10)}.Layout(gtx, func() {
+								pg.restoreBtn.Layout(gtx, pg.restoreWdg)
+							})
+						}),
+					)
+				})
 			}),
 		)
 	})
 
+	pg.drawModal(gtx)
+
+	if pg.backWdg.Clicked(gtx) {
+		return EventNav{
+			Current: RestoreID,
+			Next:    LandingID,
+		}
+	}
+
 	if pg.restoreWdg.Clicked(gtx) {
-		txt := pg.editors[1].Text()
-		fmt.Println(txt)
-		if pg.showModal {
-			pg.showModal = false
-		} else {
+		if pg.validateWords() {
 			pg.showModal = true
 		}
 	}
 
-	if pg.showModal {
-		pg.drawModal(gtx)
+	if pg.cancelWdg.Clicked(gtx) {
+		pg.showModal = false
+	}
+
+	if pg.submitWdg.Clicked(gtx) {
+		if pg.validatePwdAndPIN() {
+			return EventNav{
+				Current: RestoreID,
+				Next:    WalletsID,
+			}
+		}
 	}
 
 	return nil
 }
 
-func (pg *Restore) drawModal(gtx *layout.Context) interface{} {
+func (pg *Restore) drawModal(gtx *layout.Context) {
+	if !pg.showModal {
+		return
+	}
 	widgets := []func(){
 		func() {
 			layout.Flex{Axis: layout.Vertical}.Layout(gtx,
@@ -176,7 +213,7 @@ func (pg *Restore) drawModal(gtx *layout.Context) interface{} {
 					)
 				}),
 				layout.Rigid(func() {
-					layout.Inset{Top: unit.Dp(15)}.Layout(gtx, func() {})
+					layout.Inset{Top: unit.Dp(25)}.Layout(gtx, func() {})
 				}),
 				layout.Rigid(func() {
 					layout.Flex{Axis: layout.Vertical, Alignment: layout.Baseline}.Layout(gtx,
@@ -198,10 +235,9 @@ func (pg *Restore) drawModal(gtx *layout.Context) interface{} {
 			layout.Inset{Top: unit.Dp(15)}.Layout(gtx, func() {})
 		},
 		func() {
-			helper.PaintArea(gtx, GrayColor, gtx.Constraints.Width.Max, 1)
+			helper.PaintArea(gtx, LightGrayColor, gtx.Constraints.Width.Max, 1)
 			layout.Inset{Top: unit.Dp(15)}.Layout(gtx, func() {})
 		},
-
 		func() {
 			layout.Flex{Axis: layout.Vertical}.Layout(gtx,
 				layout.Rigid(func() {
@@ -241,12 +277,19 @@ func (pg *Restore) drawModal(gtx *layout.Context) interface{} {
 					}),
 					layout.Rigid(func() {
 						gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
-						layout.Flex{Axis: layout.Horizontal, Alignment: layout.End}.Layout(gtx,
+						layout.Flex{Axis: layout.Horizontal, Alignment: layout.Middle}.Layout(gtx,
 							layout.Rigid(func() {
 								pg.cancelBtn.Layout(gtx, pg.cancelWdg)
 							}),
 							layout.Rigid(func() {
-								pg.submitBtn.Layout(gtx, pg.submitWdg)
+								layout.Inset{Left: unit.Dp(10)}.Layout(gtx, func() {
+									pg.submitBtn.Layout(gtx, pg.submitWdg)
+								})
+							}),
+							layout.Rigid(func() {
+								layout.Inset{Left: unit.Dp(20)}.Layout(gtx, func() {
+									pg.confirmErrorMsg.Layout(gtx)
+								})
 							}),
 						)
 					}),
@@ -254,10 +297,37 @@ func (pg *Restore) drawModal(gtx *layout.Context) interface{} {
 			})
 		})
 	})
+}
 
-	if pg.cancelWdg.Clicked(gtx) || pg.submitWdg.Clicked(gtx) {
-		pg.showModal = false
+func (pg *Restore) validateWords() bool {
+	for i, editor := range pg.editors {
+		txt := editor.Text()
+		pg.inputLabels[i].Color = BlackColor
+		if txt == "" {
+			pg.inputLabels[i].Color = DangerColor
+			return false
+		}
 	}
+	return true
+}
 
-	return nil
+func (pg *modal) validatePwdAndPIN() bool {
+	pg.confirmErrorMsg.Text = ""
+	if pg.editorPassword.Text() == "" {
+		pg.confirmErrorMsg.Text = "Please enter password"
+		return false
+	}
+	if pg.editorConfirmPassword.Text() == "" {
+		pg.confirmErrorMsg.Text = "Please enter confirm password"
+		return false
+	}
+	if pg.editorConfirmPassword.Text() != pg.editorPassword.Text() {
+		pg.confirmErrorMsg.Text = "Password and confirm password do not match"
+		return false
+	}
+	if pg.editorSendingPin.Text() == "" {
+		pg.confirmErrorMsg.Text = "Please enter spending PIN"
+		return false
+	}
+	return true
 }
