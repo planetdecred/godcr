0cbc70bca5902a5e8a16cf51710026ec652e55a4
Alawode Oluwandabira <24304808+RogueElement@users.noreply.github.com>
2020-02-20 15:13:09 +0200

    Enable golint (#50)
    - Fix lint errors
    Co-authored-by: Osho Emmanuel <oshoklinsmann@gmail.com>

--- ui/values/colors.go
+++ ui/values/colors.go
@@ -3,7 +3,7 @@ package values
 import "image/color"
 
 var (
-	//  ProgressBarGray indicates the level of sync progress that is yet to be completed.
+	// ProgressBarGray indicates the level of sync progress that is yet to be completed.
 	ProgressBarGray = color.RGBA{230, 234, 237, 255}
 
 	// ProgressBarGreen indicates the level of sync progress that has been completed.
