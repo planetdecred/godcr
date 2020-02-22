9b6a02d420d862938114bdf714fe397d3810e813
codemaestro64 <42093751+codemaestro64@users.noreply.github.com>
2020-02-23 00:57:02 +0100

    add create wallet page (#32)
    * add pin and password widget
    
    * reset password form after creation
    
    * reload wallets after wallet creation
    
    * change default primary color
    
    * remove tab widget
    
    * fix button color issue
    
    * merge color struct fields into page's struct

--- ui/page/landing.go
+++ ui/page/landing.go
@@ -3,6 +3,7 @@ package page
 import (
 	"gioui.org/layout"
 	"gioui.org/text"
+	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 
@@ -18,9 +19,10 @@ const LandingID = "landing"
 // It should only be should shown if the app launches
 // and cannot find any wallets.
 type Landing struct {
-	inset     layout.Inset
-	container layout.List
-	heading   material.Label
+	inset            layout.Inset
+	container        layout.List
+	heading          material.Label
+	createErrorLabel material.Label
 
 	restoreBtn material.Button
 	restoreWdg *widget.Button
@@ -29,13 +31,23 @@ type Landing struct {
 	walletsBtn material.Button
 	walletsWdg *widget.Button
 
+	isCreatingWallet           bool
+	isShowingPasswordModal     bool
+	walletCreationSuccessEvent interface{}
+	passwordModal              *materialplus.Password
+
 	states map[string]interface{}
+	theme  *materialplus.Theme
+	wal    *wallet.Wallet
 }
 
 // Init adds a heading and two buttons.
-func (pg *Landing) Init(theme *materialplus.Theme, _ *wallet.Wallet, states map[string]interface{}) {
+func (pg *Landing) Init(theme *materialplus.Theme, wal *wallet.Wallet, states map[string]interface{}) {
 	pg.heading = theme.Label(units.Label, "Welcome to decred")
 	pg.heading.Alignment = text.Middle
+
+	pg.createErrorLabel = theme.Body2("")
+	pg.createErrorLabel.Color = theme.Danger
 
 	pg.createBtn = theme.Button("Create Wallet")
 	pg.createWdg = new(widget.Button)
@@ -48,12 +60,25 @@ func (pg *Landing) Init(theme *materialplus.Theme, _ *wallet.Wallet, states map[
 
 	pg.inset = layout.UniformInset(units.FlexInset)
 	pg.container = layout.List{Axis: layout.Vertical}
+	pg.isCreatingWallet = false
+	pg.isShowingPasswordModal = false
+	pg.walletCreationSuccessEvent = nil
+	pg.passwordModal = theme.Password()
 	pg.states = states
+	pg.theme = theme
+	pg.wal = wal
 }
 
 // Draw draws the page's to the given layout context.
 // Does not react to any event but can return a Nav event.
-func (pg *Landing) Draw(gtx *layout.Context) (res interface{}) {
+func (pg *Landing) Draw(gtx *layout.Context) interface{} {
+	ev := pg.walletCreationSuccessEvent
+	if pg.walletCreationSuccessEvent != nil {
+		pg.walletCreationSuccessEvent = nil
+	}
+
+	pg.checkForStatesUpdate()
+
 	walletInfo := pg.states[StateWalletInfo].(*wallet.MultiWalletInfo)
 	widgets := []func(){
 		func() {
@@ -61,15 +86,25 @@ func (pg *Landing) Draw(gtx *layout.Context) (res interface{}) {
 			pg.heading.Layout(gtx)
 		},
 		func() {
-			gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
-			if pg.createWdg.Clicked(gtx) {
-				log.Debugf("{%s} Create Btn clicked", LandingID)
-				// res = EventNav {
-				// 	Current: LandingID,
-				// 	Next: CreateID,
-				// }
+			topInset := float32(0)
+
+			if pg.createErrorLabel.Text != "" {
+				pg.createErrorLabel.Layout(gtx)
+				topInset += 20
 			}
-			pg.createBtn.Layout(gtx, pg.createWdg)
+
+			inset := layout.Inset{
+				Top: unit.Dp(topInset),
+			}
+			inset.Layout(gtx, func() {
+				gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
+				for pg.createWdg.Clicked(gtx) {
+					if !pg.isCreatingWallet {
+						pg.isShowingPasswordModal = !pg.isShowingPasswordModal
+					}
+				}
+				pg.createBtn.Layout(gtx, pg.createWdg)
+			})
 		},
 		func() {
 			gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
@@ -85,11 +120,13 @@ func (pg *Landing) Draw(gtx *layout.Context) (res interface{}) {
 		func() {
 			if walletInfo.LoadedWallets != 0 {
 				gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
-				if pg.walletsWdg.Clicked(gtx) {
-					log.Debugf("{%s} Wallets Btn clicked", LandingID)
-					res = EventNav{
-						Current: LandingID,
-						Next:    WalletsID,
+				for pg.walletsWdg.Clicked(gtx) {
+					if !pg.isCreatingWallet {
+						pg.reset()
+						ev = EventNav{
+							Current: LandingID,
+							Next:    WalletsID,
+						}
 					}
 				}
 				pg.walletsBtn.Layout(gtx, pg.walletsWdg)
@@ -103,5 +140,63 @@ func (pg *Landing) Draw(gtx *layout.Context) (res interface{}) {
 			layout.UniformInset(units.FlexInset).Layout(gtx, widgets[i])
 		}),
 	)
-	return
+
+	if pg.isShowingPasswordModal {
+		pg.drawPasswordModal(gtx)
+	}
+
+	return ev
+}
+
+func (pg *Landing) checkForStatesUpdate() {
+	err := pg.states[StateError]
+	created := pg.states[StateWalletCreated]
+
+	if err == nil && created == nil {
+		return
+	}
+
+	pg.reset()
+
+	if created != nil {
+		pg.passwordModal.Reset()
+		delete(pg.states, StateWalletCreated)
+
+		pg.walletCreationSuccessEvent = EventNav{
+			Current: LandingID,
+			Next:    WalletsID,
+		}
+	} else if err != nil {
+		pg.createErrorLabel.Text = err.(error).Error()
+		delete(pg.states, StateError)
+	}
+}
+
+func (pg *Landing) drawPasswordModal(gtx *layout.Context) {
+	pg.theme.Modal(gtx, func() {
+		pg.passwordModal.Draw(gtx, pg.confirm, pg.cancel)
+	})
+}
+
+func (pg *Landing) confirm(password string) {
+	pg.reset()
+
+	pg.createBtn.Text = "Creating wallet..."
+	pg.createBtn.Background = pg.theme.Disabled
+
+	pg.isCreatingWallet = true
+	pg.isShowingPasswordModal = false
+
+	pg.wal.CreateWallet(password)
+}
+
+func (pg *Landing) cancel() {
+	pg.isShowingPasswordModal = false
+}
+
+func (pg *Landing) reset() {
+	pg.isCreatingWallet = false
+	pg.createBtn.Text = "Create wallet"
+	pg.createBtn.Background = pg.theme.Color.Primary
+	pg.createErrorLabel.Text = ""
 }
