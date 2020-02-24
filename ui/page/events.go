4ee6c16c9116f352289f5985ab617ccda9a187ff
Alawode Oluwandabira <24304808+RogueElement@users.noreply.github.com>
2020-02-24 18:49:10 +0200

    ui: remove unused packages

--- ui/page/events.go
+++
@@ -1,6 +0,0 @@
-package page
-
-// EventNav is window event telling the window to go to Next page
-type EventNav struct {
-	Current, Next string
-}
--- ui/values/dimensions.go
+++
@@ -1,7 +0,0 @@
-package values
-
-var (
-	// DefaultProgressBarHeight is the default height for progress bar
-	// when non is specified.
-	DefaultProgressBarHeight = 20
-)
