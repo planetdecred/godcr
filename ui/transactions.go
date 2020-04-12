5fef78b104e0bbc8c2164f7a264eae49114a4e00
Song <song@zz.local>
2020-04-12 21:58:44 +0700

    Add more columns on transactions page layout

--- ui/transactions.go
+++ ui/transactions.go
@@ -6,6 +6,7 @@ import (
 	"gioui.org/layout"
 	"gioui.org/text"
 	"gioui.org/unit"
+	"github.com/decred/dcrd/dcrutil"
 	"github.com/raedahgroup/dcrlibwallet"
 	"github.com/raedahgroup/godcr-gio/ui/decredmaterial"
 	"github.com/raedahgroup/godcr-gio/wallet"
@@ -17,8 +18,16 @@ var (
 )
 
 const (
-	pageHeadHeight      = .2
-	pageContainerHeight = .8
+	rowDirectionWidth  = .1
+	rowDateWidth       = .2
+	rowStatusWidth     = .2
+	rowAmountWidth     = .3
+	rowFeeWidth        = .2
+	txnRowLabelSize    = 16
+	txnPageInsetTop    = 15
+	txnPageInsetLeft   = 15
+	txnPageInsetRight  = 15
+	txnPageInsetBottom = 15
 )
 
 func (win *Window) TransactionsPage() {
@@ -30,50 +39,61 @@ func (win *Window) TransactionsPage() {
 	}
 	bd := func() {
 		layout.Flex{Axis: layout.Vertical}.Layout(win.gtx,
-			layout.Flexed(pageHeadHeight, func() {
-				layout.Flex{Spacing: layout.SpaceBetween}.Layout(win.gtx,
-					layout.Rigid(func() {
-						layout.Inset{Left: unit.Dp(20)}.Layout(win.gtx, func() {
-							renderFiltererButton(win)
-						})
-					}),
-					layout.Rigid(func() {
-						win.outputs.toSend.Layout(win.gtx, &win.inputs.toSend)
-					}),
-					layout.Rigid(func() {
-						layout.Inset{Right: unit.Dp(20)}.Layout(win.gtx, func() {
-							win.outputs.toReceive.Layout(win.gtx, &win.inputs.toReceive)
-						})
-					}),
-				)
+			layout.Rigid(func() {
+				layout.Inset{Top: unit.Dp(txnPageInsetTop)}.Layout(win.gtx, func() {
+					layout.Flex{Spacing: layout.SpaceBetween}.Layout(win.gtx,
+						layout.Rigid(func() {
+							layout.Inset{Left: unit.Dp(txnPageInsetLeft)}.Layout(win.gtx, func() {
+								renderFiltererButton(win)
+							})
+						}),
+						layout.Rigid(func() {
+							win.outputs.toSend.Layout(win.gtx, &win.inputs.toSend)
+						}),
+						layout.Rigid(func() {
+							layout.Inset{Right: unit.Dp(txnPageInsetRight)}.Layout(win.gtx, func() {
+								win.outputs.toReceive.Layout(win.gtx, &win.inputs.toReceive)
+							})
+						}),
+					)
+				})
 			}),
-			layout.Flexed(pageContainerHeight, func() {
-				walletID := win.walletInfo.Wallets[win.selected].ID
-				walTxs := win.walletTransactions.Txs[walletID]
+			layout.Flexed(1, func() {
+				layout.Inset{Left: unit.Dp(txnPageInsetLeft), Right: unit.Dp(txnPageInsetRight)}.Layout(win.gtx, func() {
+					layout.Flex{Axis: layout.Vertical}.Layout(win.gtx,
+						layout.Rigid(func() {
+							layout.Inset{Top: unit.Dp(txnPageInsetTop), Bottom: unit.Dp(txnPageInsetBottom)}.Layout(win.gtx, func() {
+								rowHeader(win)
+							})
+						}),
+						layout.Flexed(1, func() {
+							walletID := win.walletInfo.Wallets[win.selected].ID
+							walTxs := win.walletTransactions.Txs[walletID]
 
-				if len(walTxs) == 0 {
-					txt := win.theme.Body1("No transactions")
-					txt.Alignment = text.Middle
-					txt.Layout(win.gtx)
-					return
-				}
+							if len(walTxs) == 0 {
+								txt := win.theme.Body1("No transactions")
+								txt.Alignment = text.Middle
+								txt.Layout(win.gtx)
+								return
+							}
 
-				var txs []wallet.TransactionInfo
-				directionFilter, _ := strconv.Atoi(win.inputs.transactionFilterDirection.Value(win.gtx))
+							var txs []wallet.TransactionInfo
+							directionFilter, _ := strconv.Atoi(win.inputs.transactionFilterDirection.Value(win.gtx))
 
-				for _, txn := range walTxs {
-					if directionFilter != 0 && txn.Txn.Direction != int32(directionFilter-1) {
-						continue
-					}
-					txs = append(txs, txn)
-				}
+							for _, txn := range walTxs {
+								if directionFilter != 0 && txn.Txn.Direction != int32(directionFilter-1) {
+									continue
+								}
+								txs = append(txs, txn)
+							}
 
-				layout.Inset{Left: unit.Dp(20), Right: unit.Dp(20)}.Layout(win.gtx, func() {
-					txsList.Layout(win.gtx, len(txs), func(index int) {
-						layout.Inset{Bottom: unit.Dp(15)}.Layout(win.gtx, func() {
-							renderTxsRow(win, txs[index])
-						})
-					})
+							txsList.Layout(win.gtx, len(txs), func(index int) {
+								layout.Inset{Bottom: unit.Dp(txnPageInsetBottom)}.Layout(win.gtx, func() {
+									renderTxsRow(win, txs[index])
+								})
+							})
+						}),
+					)
 				})
 			}),
 		)
@@ -107,36 +127,59 @@ func renderFiltererButton(win *Window) {
 	button.Layout(win.gtx, &win.inputs.toTransactionsFilters)
 }
 
+func rowHeader(win *Window) {
+	txt := win.theme.Label(unit.Dp(txnRowLabelSize), "#")
+	txt.Color = win.theme.Color.Hint
+	layout.Flex{Axis: layout.Horizontal}.Layout(win.gtx,
+		layout.Flexed(rowDirectionWidth, func() {
+			txt.Layout(win.gtx)
+		}),
+		layout.Flexed(rowDateWidth, func() {
+			txt.Text = "Date (UTC)"
+			txt.Layout(win.gtx)
+		}),
+		layout.Flexed(rowStatusWidth, func() {
+			txt.Text = "Status"
+			txt.Layout(win.gtx)
+		}),
+		layout.Flexed(rowAmountWidth, func() {
+			txt.Text = "Amount"
+			txt.Layout(win.gtx)
+		}),
+		layout.Flexed(rowFeeWidth, func() {
+			txt.Text = "Fee"
+			txt.Layout(win.gtx)
+		}),
+	)
+}
+
 func renderTxsRow(win *Window, transaction wallet.TransactionInfo) {
 	cbn := win.combined
 	initTxnWidgets(win, &transaction, &cbn)
 	layout.Flex{Axis: layout.Horizontal}.Layout(win.gtx,
-		layout.Rigid(func() {
-			layout.Inset{Left: unit.Dp(5), Top: unit.Dp(3)}.Layout(win.gtx, func() {
+		layout.Flexed(rowDirectionWidth, func() {
+			layout.Inset{Top: unit.Dp(3)}.Layout(win.gtx, func() {
 				cbn.transaction.direction.Layout(win.gtx, unit.Dp(16))
 			})
 		}),
-		layout.Flexed(1, func() {
-			layout.Inset{Left: unit.Dp(20)}.Layout(win.gtx, func() {
-				cbn.transaction.amount.Layout(win.gtx)
-			})
+		layout.Flexed(rowDateWidth, func() {
+			cbn.transaction.time.Layout(win.gtx)
 		}),
-		layout.Rigid(func() {
-			layout.Inset{Left: unit.Dp(10)}.Layout(win.gtx, func() {
-				cbn.transaction.time.Layout(win.gtx)
-			})
+		layout.Flexed(rowStatusWidth, func() {
+			win.theme.Body1(transaction.Status).Layout(win.gtx)
 		}),
-		layout.Rigid(func() {
-			layout.Inset{Bottom: unit.Dp(15), Left: unit.Dp(8), Top: unit.Dp(3)}.Layout(win.gtx, func() {
-				cbn.transaction.status.Layout(win.gtx, unit.Dp(16))
-			})
+		layout.Flexed(rowAmountWidth, func() {
+			cbn.transaction.amount.Layout(win.gtx)
+		}),
+		layout.Flexed(rowFeeWidth, func() {
+			win.theme.Body1(dcrutil.Amount(transaction.Txn.Fee).String()).Layout(win.gtx)
 		}),
 	)
 }
 
 func initTxnWidgets(win *Window, transaction *wallet.TransactionInfo, cb *combined) {
 	txWidgets := &cb.transaction
-	txWidgets.amount = win.theme.Label(unit.Dp(18), transaction.Balance)
+	txWidgets.amount = win.theme.Label(unit.Dp(16), transaction.Balance)
 	txWidgets.time = win.theme.Body1("Pending")
 
 	if transaction.Status == "confirmed" {
