bb0da23f1ecd37800001a3b74f9b3d3b1e1d6a3b
Alawode Oluwandabira <24304808+RogueElement@users.noreply.github.com>
2020-04-22 22:06:13 +0300

    ui: split wallet page layout and handling

--- ui/wallet_page.go
+++ ui/wallet_page.go
@@ -21,7 +21,7 @@ type walletPage struct {
 }
 
 func WalletPage(common pageCommon) layout.Widget {
-	page := walletPage{
+	page := &walletPage{
 		dialog: newDialogWidgets(common),
 		container: layout.List{
 			Axis: layout.Vertical,
@@ -36,115 +36,124 @@ func WalletPage(common pageCommon) layout.Widget {
 		addAcctW:      common.theme.IconButton(common.icons.contentAdd),
 		deleteW:       common.theme.DangerButton("Delete Wallet"),
 	}
-	gtx := common.gtx
 
 	return func() {
-		current := common.info.Wallets[*common.selectedWallet]
-		if common.walletsTab.Changed() {
-			page.renaming = false
-		}
-		wdgs := []func(){
-			func() {
-				if page.renaming {
-					horFlex.Layout(gtx,
-						rigid(func() {
-							page.editorW.Layout(gtx, &page.editor)
-						}),
-						rigid(func() {
-							page.renameW.Layout(gtx, &page.rename)
-						}),
-						rigid(func() {
-							layout.Center.Layout(gtx, func() {
-								page.cancelRenameW.Layout(gtx, &page.cancelRename)
-							})
-						}),
-					)
-				} else {
-					horFlex.Layout(gtx,
-						rigid(func() {
-							common.theme.H1(current.Name).Layout(common.gtx)
-						}),
-						rigid(func() {
-							layout.Center.Layout(gtx, func() {
-								page.beginRenameW.Layout(gtx, &page.beginRename)
-							})
-						}),
-					)
-				}
-			},
-			func() {
-				common.theme.H5("Total Balance: " + current.Balance).Layout(gtx)
-			},
-			func() {
+		page.Layout(common)
+		page.Handle(common)
+	}
+}
+
+func (page *walletPage) Layout(common pageCommon) {
+	gtx := common.gtx
+	current := common.info.Wallets[*common.selectedWallet]
+	if common.walletsTab.Changed() {
+		page.renaming = false
+	}
+	wdgs := []func(){
+		func() {
+			if page.renaming {
 				horFlex.Layout(gtx,
 					rigid(func() {
-						common.theme.H5("Accounts").Layout(gtx)
+						page.editorW.Layout(gtx, &page.editor)
 					}),
 					rigid(func() {
-						layout.S.Layout(gtx, func() {
-							page.addAcctW.Layout(gtx, &page.addAcct)
+						page.renameW.Layout(gtx, &page.rename)
+					}),
+					rigid(func() {
+						layout.Center.Layout(gtx, func() {
+							page.cancelRenameW.Layout(gtx, &page.cancelRename)
 						})
 					}),
 				)
-			},
-			func() {
-				page.accountsList.Layout(gtx, len(current.Accounts), func(i int) {
-					acct := current.Accounts[i]
-					a := func() {
-						layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-							layout.Rigid(func() {
-								common.theme.Body1(acct.Name).Layout(gtx)
-							}),
-							layout.Rigid(func() {
-								common.theme.Body1(acct.TotalBalance).Layout(gtx)
-							}),
-							layout.Rigid(func() {
-								common.theme.Body1("Keys: " + acct.Keys.External + " external, " + acct.Keys.Internal + " internal, " + acct.Keys.Imported + " imported").Layout(gtx)
-							}),
-							layout.Rigid(func() {
-								common.theme.Body1("HD Path: " + acct.HDPath).Layout(gtx)
-							}),
-						)
-					}
-					layout.UniformInset(unit.Dp(5)).Layout(gtx, a)
-				})
-			},
-			func() {
-				page.deleteW.Layout(gtx, &page.delete)
-			},
-		}
+			} else {
+				horFlex.Layout(gtx,
+					rigid(func() {
+						common.theme.H1(current.Name).Layout(common.gtx)
+					}),
+					rigid(func() {
+						layout.Center.Layout(gtx, func() {
+							page.beginRenameW.Layout(gtx, &page.beginRename)
+						})
+					}),
+				)
+			}
+		},
+		func() {
+			common.theme.H5("Total Balance: " + current.Balance).Layout(gtx)
+		},
+		func() {
+			horFlex.Layout(gtx,
+				rigid(func() {
+					common.theme.H5("Accounts").Layout(gtx)
+				}),
+				rigid(func() {
+					layout.S.Layout(gtx, func() {
+						page.addAcctW.Layout(gtx, &page.addAcct)
+					})
+				}),
+			)
+		},
+		func() {
+			page.accountsList.Layout(gtx, len(current.Accounts), func(i int) {
+				acct := current.Accounts[i]
+				a := func() {
+					layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+						layout.Rigid(func() {
+							common.theme.Body1(acct.Name).Layout(gtx)
+						}),
+						layout.Rigid(func() {
+							common.theme.Body1(acct.TotalBalance).Layout(gtx)
+						}),
+						layout.Rigid(func() {
+							common.theme.Body1("Keys: " + acct.Keys.External + " external, " + acct.Keys.Internal + " internal, " + acct.Keys.Imported + " imported").Layout(gtx)
+						}),
+						layout.Rigid(func() {
+							common.theme.Body1("HD Path: " + acct.HDPath).Layout(gtx)
+						}),
+					)
+				}
+				layout.UniformInset(unit.Dp(5)).Layout(gtx, a)
+			})
+		},
+		func() {
+			page.deleteW.Layout(gtx, &page.delete)
+		},
+	}
 
-		page.dialog.LayoutIfActive(gtx, func() {
-			common.LayoutWithWallets(gtx, func() {
-				page.container.Layout(common.gtx, len(wdgs), func(i int) {
-					wdgs[i]()
-				})
+	page.dialog.LayoutIfActive(gtx, func() {
+		common.LayoutWithWallets(gtx, func() {
+			page.container.Layout(common.gtx, len(wdgs), func(i int) {
+				wdgs[i]()
 			})
 		})
+	})
+}
 
-		if page.beginRename.Clicked(gtx) {
-			page.renaming = true
-			page.editor.SetText(current.Name)
-		}
+func (page *walletPage) Handle(common pageCommon) {
+	gtx := common.gtx
+	current := common.info.Wallets[*common.selectedWallet]
+	if page.beginRename.Clicked(gtx) {
+		page.renaming = true
+		page.editor.SetText(current.Name)
+	}
 
-		if page.cancelRename.Clicked(gtx) {
+	if page.cancelRename.Clicked(gtx) {
+		page.renaming = false
+	}
+
+	if page.rename.Clicked(gtx) {
+		name := page.editor.Text()
+		err := common.wallet.RenameWallet(current.ID, name)
+		if err != nil {
+			log.Error(err)
+		} else {
+			common.info.Wallets[*common.selectedWallet].Name = name
 			page.renaming = false
 		}
+	}
 
-		if page.rename.Clicked(gtx) {
-			name := page.editor.Text()
-			err := common.wallet.RenameWallet(current.ID, name)
-			if err != nil {
-				log.Error(err)
-			} else {
-				common.info.Wallets[*common.selectedWallet].Name = name
-				page.renaming = false
-			}
-		}
-
-		if page.delete.Clicked(gtx) {
-			page.dialog.SetDialog(page.dialog.deleteWallet(common))
-		}
+	if page.delete.Clicked(gtx) {
+		page.dialog.SetDialog(page.dialog.deleteWallet(common))
 	}
 }
 
