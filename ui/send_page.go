abb7a8a2197a2dc45466e00038fe978e49b2a944
codemaestro64 <code.maestro64@gmail.com>
2020-04-29 15:37:51 +0100

    remove unused code

--- ui/send_page.go
+++ ui/send_page.go
@@ -8,7 +8,7 @@ const PageSend = "send"
 
 func (win *Window) SendPage() {
 	bd := func() {
-		/**if len(win.walletInfo.Wallets) == 0 {
+		if len(win.walletInfo.Wallets) == 0 {
 			win.theme.H3("No wallets").Layout(win.gtx)
 			return
 		}
