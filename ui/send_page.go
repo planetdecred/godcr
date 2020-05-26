9ad2019c2c52519d8019e87863b9d36d38df6080
morrison <edonomorrison@gmail.com>
2020-05-26 22:42:45 +0100

    rebase wallet pr

--- ui/send_page.go
+++ ui/send_page.go
@@ -327,4 +327,286 @@ func (pg *SendPage) drawPageContents(common pageCommon) {
 			)
 		})
 	}
+	pg.drawWalletsTab(common, w)
+}
+
+func (pg *SendPage) drawSuccessSection(gtx *layout.Context) {
+	if pg.txHashText != "" {
+		layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+			layout.Flexed(0.99, func() {
+				pg.theme.SuccessAlert(gtx, pg.txHashText)
+			}),
+			layout.Rigid(func() {
+				inset := layout.Inset{
+					Left: unit.Dp(3),
+				}
+				inset.Layout(gtx, func() {
+					pg.copyIconMaterial.Layout(gtx, pg.copyIconWidget)
+				})
+			}),
+		)
+	}
+}
+
+func (pg *SendPage) drawCopiedLabelSection(gtx *layout.Context) {
+	if pg.hasCopiedTxHash {
+		pg.theme.Caption("copied").Layout(gtx)
+	}
+}
+
+func (pg *SendPage) drawSelectedAccountSection(gtx *layout.Context) {
+	layout.Center.Layout(gtx, func() {
+		layout.Stack{}.Layout(gtx,
+			layout.Stacked(func() {
+				selectedDetails := func() {
+					layout.UniformInset(unit.Dp(10)).Layout(gtx, func() {
+						layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+							layout.Rigid(func() {
+								layout.Flex{}.Layout(gtx,
+									layout.Rigid(func() {
+										layout.Inset{Bottom: unit.Dp(5)}.Layout(gtx, func() {
+											pg.theme.Body2(pg.selectedAccount.Name).Layout(gtx)
+										})
+									}),
+									layout.Rigid(func() {
+										layout.Inset{Left: unit.Dp(20)}.Layout(gtx, func() {
+											pg.theme.Body2(dcrutil.Amount(pg.selectedAccount.SpendableBalance).String()).Layout(gtx)
+										})
+									}),
+								)
+							}),
+							layout.Rigid(func() {
+								layout.Inset{Bottom: unit.Dp(5)}.Layout(gtx, func() {
+									pg.theme.Body2(pg.selectedWallet.Name).Layout(gtx)
+								})
+							}),
+						)
+					})
+				}
+				decredmaterial.Card{}.Layout(gtx, selectedDetails)
+			}),
+		)
+	})
+}
+
+func (pg *SendPage) drawTransactionDetailWidgets(gtx *layout.Context) {
+	w := []func(){
+		func() {
+			pg.tableLayoutFunc(gtx, pg.theme.Body2("Transaction Fee"), pg.transactionFeeValueLabel)
+		},
+		func() {
+			pg.tableLayoutFunc(gtx, pg.theme.Body2("Total Cost"), pg.totalCostValueLabel)
+		},
+		func() {
+			pg.tableLayoutFunc(gtx, pg.theme.Body2("Balance after send"), pg.balanceAfterSendValueLabel)
+		},
+	}
+
+	list := layout.List{Axis: layout.Vertical}
+	list.Layout(gtx, len(w), func(i int) {
+		inset := layout.Inset{
+			Top: unit.Dp(10),
+		}
+		inset.Layout(gtx, w[i])
+	})
+}
+
+func (pg *SendPage) tableLayoutFunc(gtx *layout.Context, leftLabel, rightLabel decredmaterial.Label) {
+	layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+		layout.Rigid(func() {
+			leftLabel.Layout(gtx)
+		}),
+		layout.Flexed(1, func() {
+			layout.Stack{Alignment: layout.NE}.Layout(gtx,
+				layout.Stacked(func() {
+					rightLabel.Layout(gtx)
+				}),
+			)
+		}),
+	)
+}
+
+func (pg *SendPage) drawConfirmationModal(gtx *layout.Context) {
+	w := []func(){
+		func() {
+			if pg.sendErrorText != "" {
+				gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
+				pg.theme.ErrorAlert(gtx, pg.sendErrorText)
+			}
+		},
+		func() {
+			gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
+			layout.Stack{Alignment: layout.Center}.Layout(gtx,
+				layout.Expanded(func() {
+					pg.theme.Body1(fmt.Sprintf("Sending from %s (%s)", pg.selectedAccount.Name, pg.selectedWallet.Name)).Layout(gtx)
+				}),
+			)
+		},
+		func() {
+			gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
+			layout.Stack{Alignment: layout.Center}.Layout(gtx,
+				layout.Expanded(func() {
+					pg.theme.Body2("To destination address").Layout(gtx)
+				}),
+			)
+		},
+		func() {
+			gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
+			layout.Stack{Alignment: layout.Center}.Layout(gtx,
+				layout.Expanded(func() {
+					pg.theme.Body1(pg.destinationAddressEditor.Text()).Layout(gtx)
+				}),
+			)
+		},
+		func() {
+			pg.drawTransactionDetailWidgets(gtx)
+		},
+		func() {
+			gtx.Constraints.Width.Min = gtx.Constraints.Width.Max
+			layout.Stack{Alignment: layout.Center}.Layout(gtx,
+				layout.Expanded(func() {
+					pg.theme.Caption("Your DCR will be sent and CANNOT be undone").Layout(gtx)
+				}),
+			)
+		},
+		func() {
+			layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+				layout.Rigid(func() {
+					pg.confirmButtonMaterial.Layout(gtx, pg.confirmButtonWidget)
+				}),
+				layout.Rigid(func() {
+					inset := layout.Inset{
+						Left: unit.Dp(5),
+					}
+					inset.Layout(gtx, func() {
+						pg.closeConfirmationModalButtonMaterial.Layout(gtx, pg.closeConfirmationModalButtonWidget)
+					})
+				}),
+			)
+		},
+	}
+	pg.theme.Modal(gtx, "Confirm Send Transaction", w)
+}
+
+func (pg *SendPage) drawPasswordModal(gtx *layout.Context) {
+	pg.passwordModal.Layout(gtx, func(password []byte) {
+		pg.isBroadcastingTransaction = true
+		pg.isPasswordModalOpen = false
+
+		pg.wallet.BroadcastTransaction(pg.txAuthor, password, pg.broadcastErrChan)
+	}, func() {
+		pg.isPasswordModalOpen = false
+	})
+}
+
+func (pg *SendPage) validate(ignoreEmpty bool) bool {
+	isAddressValid := pg.validateDestinationAddress(ignoreEmpty)
+	isAmountValid := pg.validateAmount(ignoreEmpty)
+
+	if !isAddressValid || !isAmountValid || pg.calculateErrorText != "" {
+		pg.nextButtonMaterial.Background = pg.theme.Color.Hint
+		return false
+	}
+
+	pg.nextButtonMaterial.Background = pg.theme.Color.Primary
+	return true
+}
+
+func (pg *SendPage) validateDestinationAddress(ignoreEmpty bool) bool {
+	pg.destinationAddressEditorMaterial.ClearError()
+	destinationAddress := pg.destinationAddressEditor.Text()
+	if destinationAddress == "" && !ignoreEmpty {
+		pg.destinationAddressEditorMaterial.SetError("please enter a destination address")
+		return false
+	}
+
+	if destinationAddress != "" {
+		isValid, _ := pg.wallet.IsAddressValid(destinationAddress)
+		if !isValid {
+			pg.destinationAddressEditorMaterial.SetError("invalid address")
+			return false
+		}
+	}
+	return true
+}
+
+func (pg *SendPage) validateAmount(ignoreEmpty bool) bool {
+	pg.sendAmountEditorMaterial.ClearError()
+	amount := pg.sendAmountEditor.Text()
+	if amount == "" {
+		if !ignoreEmpty {
+			pg.sendAmountEditorMaterial.SetError("please enter a send amount")
+		}
+		return false
+	}
+
+	if amount != "" {
+		_, err := strconv.ParseFloat(amount, 64)
+		if err != nil {
+			pg.sendAmountEditorMaterial.SetError("please enter a valid amount")
+			return false
+		}
+	}
+
+	return true
+}
+
+func (pg *SendPage) calculateValues() {
+	pg.transactionFeeValueLabel.Text = "0 DCR"
+	pg.totalCostValueLabel.Text = "0 DCR "
+	pg.calculateErrorText = ""
+
+	pg.balanceAfterSendValueLabel.Text = dcrutil.Amount(pg.selectedWallet.SpendableBalance).String()
+
+	if pg.txAuthor == nil || !pg.validate(true) {
+		return
+	}
+
+	amountDCR, _ := strconv.ParseFloat(pg.sendAmountEditor.Text(), 64)
+	amount, err := dcrutil.NewAmount(amountDCR)
+	if err != nil {
+		pg.calculateErrorText = fmt.Sprintf("error estimating transaction fee: %s", err)
+		return
+	}
+	amountAtoms := int64(amount)
+
+	// set destination address
+	pg.txAuthor.RemoveSendDestination(0)
+	pg.txAuthor.AddSendDestination(pg.destinationAddressEditor.Text(), amountAtoms, false)
+
+	// calculate transaction fee
+	feeAndSize, err := pg.txAuthor.EstimateFeeAndSize()
+	if err != nil {
+		pg.calculateErrorText = fmt.Sprintf("error estimating transaction fee: %s", err)
+		return
+	}
+
+	txFee := feeAndSize.Fee.AtomValue
+	totalCost := txFee + amountAtoms
+
+	pg.remainingBalance = pg.selectedWallet.SpendableBalance - totalCost
+	pg.transactionFeeValueLabel.Text = dcrutil.Amount(txFee).String()
+	pg.totalCostValueLabel.Text = dcrutil.Amount(totalCost).String()
+	pg.balanceAfterSendValueLabel.Text = dcrutil.Amount(pg.remainingBalance).String()
+}
+
+func (pg *SendPage) watchForBroadcastResult() {
+	if pg.broadcastResult == nil {
+		return
+	}
+
+	if pg.broadcastResult.TxHash != "" {
+		if pg.remainingBalance != -1 {
+			pg.selectedAccount.SpendableBalance = pg.remainingBalance
+		}
+		pg.remainingBalance = -1
+
+		pg.txHash = pg.broadcastResult.TxHash
+		pg.txHashText = fmt.Sprintf("Successful. Hash: %s", pg.broadcastResult.TxHash)
+		pg.destinationAddressEditor.SetText("")
+		pg.sendAmountEditor.SetText("")
+		pg.isConfirmationModalOpen = false
+		pg.isBroadcastingTransaction = false
+		pg.broadcastResult.TxHash = ""
+	}
 }
