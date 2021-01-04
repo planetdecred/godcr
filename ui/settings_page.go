4e0e700461cbaf946cb2d2f14a52f5cd2bd49a9a
morrison <edonomorrison@gmail.com>
2021-01-04 09:20:18 +0100

    add basic setting page infastructure

--- ui/settings_page.go
+++ ui/settings_page.go
@@ -4,6 +4,7 @@ import (
 	"gioui.org/layout"
 
 	"github.com/planetdecred/godcr/ui/decredmaterial"
+	"github.com/planetdecred/godcr/ui/values"
 )
 
 const PageSettings = "Settings"
@@ -25,11 +26,37 @@ func (win *Window) SettingsPage(common pageCommon) layout.Widget {
 
 // main settings layout
 func (pg *settingsPage) Layout(gtx layout.Context, common pageCommon) layout.Dimensions {
-	return common.Layout(gtx, func(gtx C) D {
-		return layout.Center.Layout(gtx, func(gtx C) D {
-			return pg.dummyText.Layout(gtx)
-		})
-	})
+	body := func(gtx C) D {
+		page := SubPage{
+			title:      "Settings",
+			walletName: common.info.Wallets[*common.selectedWallet].Name,
+			back: func() {
+				*common.page = PageWallet
+			},
+			body: func(gtx layout.Context) layout.Dimensions {
+				return layout.UniformInset(values.MarginPadding5).Layout(gtx, func(gtx C) D {
+					return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+						layout.Rigid(func(gtx C) D {
+							inset := layout.Inset{
+								Left: values.MarginPadding10,
+							}
+							return inset.Layout(gtx, func(gtx C) D {
+								return pg.dummyText.Layout(gtx)
+							})
+						}),
+						// layout.Rigid(pg.editors(pg.addressEditor)),
+						// layout.Rigid(pg.editors(pg.messageEditor)),
+						// layout.Rigid(pg.drawButtonsRow()),
+						// layout.Rigid(pg.drawResult()),
+					)
+				})
+			},
+			infoTemplate: "",
+		}
+		return common.SubPageLayout(gtx, page)
+	}
+
+	return common.Layout(gtx, body)
 }
 
 func (pg *settingsPage) handle(common pageCommon) {
