65e8a138f5659964b55e8c689c93981789d05b5e
Collins Olanrewaju <ocollins444@gmail.com>
2021-05-18 19:06:51 +0100

    Add Chinese translation with fallback support to other languages

---
+++ ui/values/localizable/zh.go
@@ -0,0 +1,32 @@
+package localizable
+
+const ZH = `
+"seeAll" = "查看所有";
+"receive" = "接收";
+"blockHeadersFetched" = "区块头已获取";
+"scanningTotalHeaders" = "%d/%d";
+"unlock" = "解锁";
+"noConnectedPeer" = "未连接节点";
+"connectedTo" = "已连接%d个节点";
+"synchronizing" = "正在同步...";
+"walletNotSynced" = "未同步";
+"recentTransactions" = "最近的交易";
+"send" = "发送";
+"hideDetails" = "隐藏详情";
+"connectedPeersCount" = "连接节点数";
+"disconnect" = "断开连接";
+"unlockToResumeRestoration" = "解锁以恢复";
+"offline" = "离线";
+"reconnect" = "重新连接";
+"currentTotalBalance" = "总余额";
+"walletStatus" = "钱包状态";
+"walletSynced" = "已同步";
+"cancel" = "取消";
+"showDetails" = "显示详情";
+"syncSteps" = "步骤 %d/3";
+"syncingProgress" = "当前同步进度";
+"online" = "在线";
+"noTransactions" = "没有交易";
+"headersFetchProgress" = "获取区块头 · %d%%";
+
+`
--- ui/values/strings.go
+++ ui/values/strings.go
@@ -9,30 +9,52 @@ import (
 	"github.com/planetdecred/godcr/ui/values/localizable"
 )
 
-var rex = regexp.MustCompile(`(?m)("(?:\\.|[^"\\])*")\s+=\s+("(?:\\.|[^"\\])*")`) // "key"="value"
-var Languages = []string{"en"}
+var rex = regexp.MustCompile(`(?m)("(?:\\.|[^"\\])*")\s*=\s*("(?:\\.|[^"\\])*")`) // "key"="value"
+var Languages = []string{"en", "zh"}
+var UserLanguages = []string{"en"} // order of preference
 
-const DefaultLanguge = "en"
+const (
+	DefaultLanguge = "en"
+	commentPrefix  = "/"
+)
 
 var en map[string]string
+var zh map[string]string
+
+var languageStrings map[string]map[string]string
 
 func init() {
-	en = make(map[string]string)
-	scanner := bufio.NewScanner(strings.NewReader(localizable.EN))
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.TrimSpace(line) == "" {
-			continue
-		}
-		kv := rex.FindAllStringSubmatch(line, -1)[0]
-		key := trimQuotes(kv[1])
-		value := trimQuotes(kv[2])
 
-		en[key] = value
+	readIntoMap := func(m map[string]string, localizableStrings string) {
+		scanner := bufio.NewScanner(strings.NewReader(localizableStrings))
+		for scanner.Scan() {
+			line := scanner.Text()
+			line = strings.TrimSpace(line)
+			if line == "" || strings.HasPrefix(line, commentPrefix) {
+				continue
+			}
+
+			matches := rex.FindAllStringSubmatch(line, -1)
+			if len(matches) == 0 {
+				continue
+			}
+
+			kv := matches[0]
+			key := trimQuotes(kv[1])
+			value := trimQuotes(kv[2])
+
+			m[key] = value
+		}
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println("localizable: error reading scanner input:", err)
-	}
+
+	en = make(map[string]string)
+	zh = make(map[string]string)
+	languageStrings = make(map[string]map[string]string)
+
+	readIntoMap(en, localizable.EN)
+	languageStrings["en"] = en
+	readIntoMap(zh, localizable.ZH)
+	languageStrings["zh"] = zh
 }
 
 func trimQuotes(s string) string {
@@ -45,12 +67,15 @@ func trimQuotes(s string) string {
 }
 
 func String(key string) string {
-	str, ok := en[key]
-	if !ok {
-		return ""
+	for _, lang := range UserLanguages {
+		languageMap := languageStrings[lang]
+		str, ok := languageMap[key]
+		if ok {
+			return str
+		}
 	}
 
-	return str
+	return ""
 }
 
 func StringF(key string, a ...interface{}) string {
