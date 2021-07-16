554e771eab0cec196804d7eae9051f38f122c8dc
morrison <edonomorrison@gmail.com>
2021-07-16 08:32:07 +0100

    rebase pr and clean up code

--- ui/listeners.go
+++ ui/listeners.go
@@ -19,11 +19,13 @@ func (mp *mainPage) OnTransaction(transaction string) {
 	var tx dcrlibwallet.Transaction
 	err := json.Unmarshal([]byte(transaction), &tx)
 	if err == nil {
-		mp.updateNotification(wallet.NewTransaction{
+		update := wallet.NewTransaction{
 			Transaction: &tx,
-		})
+		}
+		mp.updateNotification(update)
+		mp.desktopNotifier(update)
 	}
-	mp.desktopNotifier()
+
 }
 
 func (mp *mainPage) OnBlockAttached(walletID int, blockHeight int32) {
@@ -49,26 +51,29 @@ func (mp *mainPage) OnProposalsSynced() {
 }
 
 func (mp *mainPage) OnNewProposal(proposal *dcrlibwallet.Proposal) {
-	mp.notificationsUpdate <- wallet.Proposal{
+	update := wallet.Proposal{
 		ProposalStatus: wallet.NewProposalFound,
 		Proposal:       proposal,
 	}
-	mp.desktopNotifier()
+	mp.notificationsUpdate <- update
+	mp.desktopNotifier(update)
 }
 
 func (mp *mainPage) OnProposalVoteStarted(proposal *dcrlibwallet.Proposal) {
-	mp.notificationsUpdate <- wallet.Proposal{
+	update := wallet.Proposal{
 		ProposalStatus: wallet.VoteStarted,
 		Proposal:       proposal,
 	}
-	mp.desktopNotifier()
+	mp.notificationsUpdate <- update
+	mp.desktopNotifier(update)
 }
 func (mp *mainPage) OnProposalVoteFinished(proposal *dcrlibwallet.Proposal) {
-	mp.notificationsUpdate <- wallet.Proposal{
+	update := wallet.Proposal{
 		ProposalStatus: wallet.VoteFinished,
 		Proposal:       proposal,
 	}
-	mp.desktopNotifier()
+	mp.notificationsUpdate <- update
+	mp.desktopNotifier(update)
 }
 
 // Sync notifications
@@ -145,98 +150,85 @@ func (mp *mainPage) updateNotification(signal interface{}) {
 		mp.load.Receiver.NotificationsUpdate <- signal
 	default:
 		mp.notificationsUpdate <- signal
-		}
+	}
 }
 
-func (mp *mainPage) desktopNotifier() {
-	go func() {
-		for {
-			var notifier interface{}
+func (mp *mainPage) desktopNotifier(notifier interface{}) {
+	var notification string
+	switch t := notifier.(type) {
+	case wallet.NewTransaction:
+		// remove trailing zeros from amount and convert to string
+		amount := strconv.FormatFloat(dcrlibwallet.AmountCoin(t.Transaction.Amount), 'f', -1, 64)
 
-			select {
-			case notifier = <-mp.notificationsUpdate:
+		defaultNotification := "Transaction notification of %s DCR"
+		notificationText := "You have %s %s DCR "
+
+		wallet := mp.pageCommon.multiWallet.WalletWithID(t.Transaction.WalletID)
+		if wallet == nil {
+			notification = fmt.Sprintf(defaultNotification, amount)
+			return
+		}
+
+		getAccount := func(acct int32) string {
+			var account string
+			accountName, err := wallet.AccountName(acct)
+			if err != nil {
+				log.Error(err)
+			} else {
+				account = accountName
 			}
+			return account
+		}
 
-			var notification string
-			switch t := notifier.(type) {
-			case wallet.NewTransaction:
-				// remove trailing zeros from amount and convert to string
-				amount := strconv.FormatFloat(dcrlibwallet.AmountCoin(t.Transaction.Amount), 'f', -1, 64)
-
-				defaultNotification := "Transaction notification of %s DCR"
-				notificationText := "You have %s %s DCR "
-
-				wallet := mp.pageCommon.multiWallet.WalletWithID(t.Transaction.WalletID)
-				if wallet == nil {
-					notification = fmt.Sprintf(defaultNotification, amount)
-					return
+		// get source account
+		var txSourceAccount string
+		if t.Transaction.Direction == dcrlibwallet.TxDirectionSent ||
+			t.Transaction.Direction == dcrlibwallet.TxDirectionTransferred {
+			for _, input := range t.Transaction.Inputs {
+				if input.AccountNumber != -1 {
+					txSourceAccount = getAccount(input.AccountNumber)
 				}
-
-				getAccount := func(acct int32) string {
-					var account string
-					accountName, err := wallet.AccountName(acct)
-					if err != nil {
-						log.Error(err)
-					} else {
-						account = accountName
-					}
-					return account
-				}
-
-				// get source account
-				var txSourceAccount string
-				if t.Transaction.Direction == dcrlibwallet.TxDirectionSent ||
-					t.Transaction.Direction == dcrlibwallet.TxDirectionTransferred {
-					for _, input := range t.Transaction.Inputs {
-						if input.AccountNumber != -1 {
-							txSourceAccount = getAccount(input.AccountNumber)
-						}
-					}
-				}
-
-				//get distination account
-				var txDestAccount, txDestinationAddress string
-				if t.Transaction.Direction == dcrlibwallet.TxDirectionTransferred ||
-					t.Transaction.Direction == dcrlibwallet.TxDirectionReceived ||
-					t.Transaction.Direction == dcrlibwallet.TxDirectionSent {
-					for _, output := range t.Transaction.Outputs {
-						if output.AccountNumber != -1 {
-							txDestAccount = getAccount(output.AccountNumber)
-						}
-						if output.AccountNumber == -1 {
-							txDestinationAddress = output.Address
-						}
-					}
-				}
-
-				switch {
-				case t.Transaction.Direction == dcrlibwallet.TxDirectionReceived:
-					notification = fmt.Sprintf(notificationText+"to %s account in %s wallet.", "received", amount, txDestAccount, wallet.Name)
-				case t.Transaction.Direction == dcrlibwallet.TxDirectionSent:
-					notification = fmt.Sprintf(notificationText+"from %s account in %s wallet to %s.", "sent", amount, wallet.Name, txSourceAccount, txDestinationAddress)
-				case t.Transaction.Direction == dcrlibwallet.TxDirectionTransferred:
-					notification = fmt.Sprintf(notificationText+"from %s account, to %s account in %s wallet.", "transferred", amount, txSourceAccount, txDestAccount, wallet.Name)
-				default:
-					notification = fmt.Sprintf(defaultNotification+" to/from (%s wallet) account %s", amount, wallet.Name, txSourceAccount)
-				}
-				initializeBeepNotification(notification)
-			case wallet.Proposal:
-				notification = fmt.Sprintf("Net sure what i stored here : %T", t)
-				fmt.Printf("Net sure what i stored here : %T", t)
-				switch {
-				case t.ProposalStatus == wallet.NewProposalFound:
-					notification = fmt.Sprintf("A new proposal has been added Token: %s", t.Proposal.Token)
-				case t.ProposalStatus == wallet.VoteStarted:
-					notification = fmt.Sprintf("Voting has started for proposal with Token: %s", t.Proposal.Token)
-				case t.ProposalStatus == wallet.VoteFinished:
-					notification = fmt.Sprintf("Voting has ended for proposal with Token: %s", t.Proposal.Token)
-				default:
-					notification = fmt.Sprintf("New update for proposal with Token: %s", t.Proposal.Token)
-				}
-				initializeBeepNotification(notification)
 			}
 		}
-	}()
+
+		//get distination account
+		var txDestAccount, txDestinationAddress string
+		if t.Transaction.Direction == dcrlibwallet.TxDirectionTransferred ||
+			t.Transaction.Direction == dcrlibwallet.TxDirectionReceived ||
+			t.Transaction.Direction == dcrlibwallet.TxDirectionSent {
+			for _, output := range t.Transaction.Outputs {
+				if output.AccountNumber != -1 {
+					txDestAccount = getAccount(output.AccountNumber)
+				}
+				if output.AccountNumber == -1 {
+					txDestinationAddress = output.Address
+				}
+			}
+		}
+		switch {
+		case t.Transaction.Direction == dcrlibwallet.TxDirectionReceived:
+			notification = fmt.Sprintf(notificationText+"to %s account in %s wallet.", "received", amount, txDestAccount, wallet.Name)
+		case t.Transaction.Direction == dcrlibwallet.TxDirectionSent:
+			notification = fmt.Sprintf(notificationText+"from %s account in %s wallet to %s.", "sent", amount, wallet.Name, txSourceAccount, txDestinationAddress)
+		case t.Transaction.Direction == dcrlibwallet.TxDirectionTransferred:
+			notification = fmt.Sprintf(notificationText+"from %s account, to %s account in %s wallet.", "transferred", amount, txSourceAccount, txDestAccount, wallet.Name)
+		default:
+			notification = fmt.Sprintf(defaultNotification+" to/from (%s wallet) account %s", amount, wallet.Name, txSourceAccount)
+		}
+		initializeBeepNotification(notification)
+	case wallet.Proposal:
+		switch {
+		case t.ProposalStatus == wallet.NewProposalFound:
+			notification = fmt.Sprintf("A new proposal has been added Token: %s", t.Proposal.Token)
+		case t.ProposalStatus == wallet.VoteStarted:
+			notification = fmt.Sprintf("Voting has started for proposal with Token: %s", t.Proposal.Token)
+		case t.ProposalStatus == wallet.VoteFinished:
+			notification = fmt.Sprintf("Voting has ended for proposal with Token: %s", t.Proposal.Token)
+		default:
+			notification = fmt.Sprintf("New update for proposal with Token: %s", t.Proposal.Token)
+		}
+		initializeBeepNotification(notification)
+	}
 }
 
 func initializeBeepNotification(n string) {
