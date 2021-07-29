7c660d75616e32b3120d1dd6b28031161241dd5d
Sirmorrison <Edonomorrison@gmail.com>
2021-07-29 13:43:12 +0000

    add custom switch widget (#487)
    - add disabled state
    
    - use widget.Clickable instead of widget.Bool

--- ui/decredmaterial/theme.go
+++ ui/decredmaterial/theme.go
@@ -281,3 +281,28 @@ func (t *Theme) closeAllDropdownMenus(group uint) {
 		}
 	}
 }
+
+// Disabled blends color towards the luminance and multiplies alpha.
+// Blending towards luminance will desaturate the color.
+// Multiplying alpha blends the color together more with the background.
+func Disabled(c color.NRGBA) (d color.NRGBA) {
+	const r = 80 // blend ratio
+	lum := approxLuminance(c)
+	return color.NRGBA{
+		R: byte((int(c.R)*r + int(lum)*(256-r)) / 256),
+		G: byte((int(c.G)*r + int(lum)*(256-r)) / 256),
+		B: byte((int(c.B)*r + int(lum)*(256-r)) / 256),
+		A: byte(int(c.A) * (128 + 32) / 256),
+	}
+}
+
+// approxLuminance is a fast approximate version of RGBA.Luminance.
+func approxLuminance(c color.NRGBA) byte {
+	const (
+		r = 13933 // 0.2126 * 256 * 256
+		g = 46871 // 0.7152 * 256 * 256
+		b = 4732  // 0.0722 * 256 * 256
+		t = r + g + b
+	)
+	return byte((r*int(c.R) + g*int(c.G) + b*int(c.B)) / t)
+}
