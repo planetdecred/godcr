5341d7fcb44cb98c402115d14308dbed7e0dc404
Olanrewaju Collins <ocollins444@gmail.com>
2021-08-21 10:05:36 +0100

    Add LinearLayout (#563)
    * Add LinearLayout
    
    LinearLayout serves as a wrapper for several gio layout structs and
    it reduces the amount of code required when applying basic attributes
    like padding, margin, border, etc to a layout.
    
    * Fix lint
    
    * Fix typo in comment
    
    * Use linear layout in more page

---
+++ ui/decredmaterial/linearlayout.go
@@ -0,0 +1,78 @@
+package decredmaterial
+
+import (
+	"image/color"
+
+	"gioui.org/f32"
+	"gioui.org/layout"
+	"gioui.org/op/clip"
+	"gioui.org/unit"
+)
+
+const (
+	WrapContent = -1
+	MatchParent = -2
+)
+
+type LinearLayout struct {
+	Width       int
+	Height      int
+	Orientation layout.Axis
+	Background  color.NRGBA
+	Border      Border
+	Margin      layout.Inset
+	Padding     layout.Inset
+	Direction   layout.Direction
+}
+
+func (ll LinearLayout) Layout(gtx C, children ...layout.FlexChild) D {
+	// draw margin
+	return ll.Margin.Layout(gtx, func(gtx C) D {
+		return layout.Stack{}.Layout(gtx,
+			layout.Expanded(func(gtx C) D {
+				ll.applyDimension(&gtx)
+				// draw background and clip the background to border radius
+				tr := float32(gtx.Px(unit.Dp(ll.Border.Radius.TopRight)))
+				tl := float32(gtx.Px(unit.Dp(ll.Border.Radius.TopLeft)))
+				br := float32(gtx.Px(unit.Dp(ll.Border.Radius.BottomRight)))
+				bl := float32(gtx.Px(unit.Dp(ll.Border.Radius.BottomLeft)))
+				clip.RRect{
+					Rect: f32.Rectangle{Max: f32.Point{
+						X: float32(gtx.Constraints.Min.X),
+						Y: float32(gtx.Constraints.Min.Y),
+					}},
+					NW: tl, NE: tr, SE: br, SW: bl,
+				}.Add(gtx.Ops)
+				return fill(gtx, ll.Background)
+			}),
+			layout.Stacked(func(gtx C) D {
+				ll.applyDimension(&gtx)
+				return ll.Border.Layout(gtx, func(gtx C) D {
+					// draw padding
+					return ll.Padding.Layout(gtx, func(gtx C) D {
+						// draw layout direction
+						return ll.Direction.Layout(gtx, func(gtx C) D {
+							return layout.Flex{Axis: ll.Orientation}.Layout(gtx, children...)
+						})
+					})
+				})
+			}),
+		)
+	})
+}
+
+func (ll LinearLayout) applyDimension(gtx *C) {
+	if ll.Width == MatchParent {
+		gtx.Constraints.Min.X = gtx.Constraints.Max.X
+	} else if ll.Width != WrapContent {
+		gtx.Constraints.Min.X = ll.Width
+		gtx.Constraints.Max.X = ll.Width
+	}
+
+	if ll.Height == MatchParent {
+		gtx.Constraints.Min.Y = gtx.Constraints.Max.Y
+	} else if ll.Height != WrapContent {
+		gtx.Constraints.Min.Y = ll.Height
+		gtx.Constraints.Max.Y = ll.Height
+	}
+}
--- ui/page/more_page.go
+++ ui/page/more_page.go
@@ -108,47 +108,36 @@ func (pg *MorePage) Layout(gtx layout.Context) layout.Dimensions {
 }
 
 func (pg *MorePage) layoutMoreItems(gtx layout.Context) layout.Dimensions {
-	return layout.Stack{}.Layout(gtx,
-		layout.Stacked(func(gtx C) D {
-			list := layout.List{Axis: layout.Vertical}
-			return list.Layout(gtx, len(pg.morePageListItems), func(gtx C, i int) D {
-				return layout.Inset{Bottom: values.MarginPadding5}.Layout(gtx, func(gtx C) D {
-					return decredmaterial.Clickable(gtx, pg.morePageListItems[i].clickable, func(gtx C) D {
-						background := pg.Theme.Color.Surface
-						card := pg.Theme.Card()
-						card.Color = background
-						return card.Layout(gtx, func(gtx C) D {
-							gtx.Constraints.Min.X = gtx.Constraints.Max.X
-							return layout.Stack{}.Layout(gtx,
-								layout.Stacked(func(gtx C) D {
-									return layout.UniformInset(values.MarginPadding15).Layout(gtx, func(gtx C) D {
-										gtx.Constraints.Min.X = gtx.Constraints.Max.X
-										return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
-											layout.Rigid(func(gtx C) D {
-												return layout.Center.Layout(gtx, pg.morePageListItems[i].image.Layout)
-											}),
-											layout.Rigid(func(gtx C) D {
-												return layout.Inset{
-													Left: values.MarginPadding15,
-													Top:  values.MarginPadding2,
-												}.Layout(gtx, func(gtx C) D {
-													return layout.Center.Layout(gtx, func(gtx C) D {
-														page := pg.morePageListItems[i].page
-														if page == SecurityToolsPageID {
-															page = "Security Tools"
-														}
-														return pg.Theme.Body1(page).Layout(gtx)
-													})
-												})
-											}),
-										)
-									})
-								}),
-							)
+
+	list := layout.List{Axis: layout.Vertical}
+	return list.Layout(gtx, len(pg.morePageListItems), func(gtx C, i int) D {
+		return layout.Inset{Bottom: values.MarginPadding5}.Layout(gtx, func(gtx C) D {
+			return decredmaterial.Clickable(gtx, pg.morePageListItems[i].clickable, func(gtx C) D {
+				return decredmaterial.LinearLayout{Orientation: layout.Horizontal,
+					Width:      decredmaterial.MatchParent,
+					Height:     decredmaterial.WrapContent,
+					Background: pg.Theme.Color.Surface,
+					Border:     decredmaterial.Border{Radius: decredmaterial.Radius(14)},
+					Padding:    layout.UniformInset(values.MarginPadding15)}.Layout(gtx,
+					layout.Rigid(func(gtx C) D {
+						return layout.Center.Layout(gtx, pg.morePageListItems[i].image.Layout)
+					}),
+					layout.Rigid(func(gtx C) D {
+						return layout.Inset{
+							Left: values.MarginPadding15,
+							Top:  values.MarginPadding2,
+						}.Layout(gtx, func(gtx C) D {
+							return layout.Center.Layout(gtx, func(gtx C) D {
+								page := pg.morePageListItems[i].page
+								if page == SecurityToolsPageID {
+									page = "Security Tools"
+								}
+								return pg.Theme.Body1(page).Layout(gtx)
+							})
 						})
-					})
-				})
+					}),
+				)
 			})
-		}),
-	)
+		})
+	})
 }
