ebc14c8e9269038d1b5a95eecf084b59612d4b38
devchoplife <dritedurojaiye@gmail.com>
2021-08-25 08:04:22 +0100

    Set individual padding for element

--- ui/page/about_page.go
+++ ui/page/about_page.go
@@ -7,33 +7,12 @@ import (
 	"github.com/planetdecred/godcr/ui/decredmaterial"
 	"github.com/planetdecred/godcr/ui/load"
 	"github.com/planetdecred/godcr/ui/page/components"
+	"github.com/planetdecred/godcr/ui/values"
 )
 
 const AboutPageID = "About"
 
-type unclickableRow struct {
-	leftWidget  decredmaterial.Label
-	rightWidget decredmaterial.Label
-}
-
-type clickableLicense struct {
-	clickable   *widget.Clickable
-	leftWidget  decredmaterial.Label
-	rightWidget *widget.Icon
-}
-
 type AboutPage struct {
-	*load.Load
-	container    *layout.List
-	card         decredmaterial.Card
-	versionRow   unclickableRow
-	buildDateRow unclickableRow
-	networkRow   unclickableRow
-	licenseRow   clickableLicense
-	backButton   decredmaterial.IconButton
-}
-
-/*type AboutPage struct {
 	*load.Load
 	card      decredmaterial.Card
 	container *layout.List
@@ -51,44 +30,28 @@ type AboutPage struct {
 
 	backButton decredmaterial.IconButton
 }
-*/
 
-func NewAboutPage2(l *load.Load) *AboutPage {
-	versionRow := unclickableRow{
-		leftWidget:  l.Theme.Body1("Version"),
-		rightWidget: l.Theme.Body1("v1.5.2"),
-	}
-
-	buildDateRow := unclickableRow{
-		leftWidget:  l.Theme.Body1("Build date"),
-		rightWidget: l.Theme.Body1("2020-09-10"),
-	}
-
-	networkRow := unclickableRow{
-		leftWidget:  l.Theme.Body1("Network"),
-		rightWidget: l.Theme.Body1(l.WL.Wallet.Net),
-	}
-
-	licenseRow := clickableLicense{
-		clickable:   new(widget.Clickable),
-		leftWidget:  l.Theme.Body1("License"),
-		rightWidget: l.Icons.ChevronRight,
-	}
+func NewAboutPage(l *load.Load) *AboutPage {
 	pg := &AboutPage{
-		Load:         l,
-		card:         l.Theme.Card(),
-		container:    &layout.List{Axis: layout.Vertical},
-		versionRow:   versionRow,
-		buildDateRow: buildDateRow,
-		networkRow:   networkRow,
-		licenseRow:   licenseRow,
+		Load:             l,
+		card:             l.Theme.Card(),
+		container:        &layout.List{Axis: layout.Vertical},
+		version:          l.Theme.Body1("Version"),
+		versionValue:     l.Theme.Body1("v1.5.2"),
+		buildDate:        l.Theme.Body1("Build date"),
+		buildDateValue:   l.Theme.Body1("2020-09-10"),
+		network:          l.Theme.Body1("Network"),
+		networkValue:     l.Theme.Body1(l.WL.Wallet.Net),
+		license:          l.Theme.Body1("License"),
+		licenseRow:       new(widget.Clickable),
+		chevronRightIcon: l.Icons.ChevronRight,
 	}
 
 	pg.backButton, _ = components.SubpageHeaderButtons(l)
-	pg.versionRow.rightWidget.Color = pg.Theme.Color.Gray
-	pg.buildDateRow.rightWidget.Color = pg.Theme.Color.Gray
-	pg.networkRow.rightWidget.Color = pg.Theme.Color.Gray
-	pg.licenseRow.rightWidget.Color = pg.Theme.Color.Gray
+	pg.versionValue.Color = pg.Theme.Color.Gray
+	pg.buildDateValue.Color = pg.Theme.Color.Gray
+	pg.networkValue.Color = pg.Theme.Color.Gray
+	pg.chevronRightIcon.Color = pg.Theme.Color.Gray
 
 	return pg
 }
@@ -112,7 +75,7 @@ func (pg *AboutPage) Layout(gtx layout.Context) layout.Dimensions {
 			},
 			Body: func(gtx C) D {
 				return pg.card.Layout(gtx, func(gtx C) D {
-					return pg.Layout(gtx)
+					return pg.layoutRows(gtx)
 				})
 			},
 		}
@@ -122,24 +85,65 @@ func (pg *AboutPage) Layout(gtx layout.Context) layout.Dimensions {
 	return components.UniformPadding(gtx, body)
 }
 
-/*func (pg *AboutPage) layoutRows(gtx layout.Context) layout.Dimensions {
+func (pg *AboutPage) layoutRows(gtx layout.Context) layout.Dimensions {
 	w := []func(gtx C) D{
 		func(gtx C) D {
-			return components.EndToEndRow(gtx, pg.version.Layout, pg.versionValue.Layout)
+			return components.Container{
+				Padding: layout.Inset{
+					Top:    values.MarginPadding20,
+					Bottom: values.MarginPadding20,
+					Left:   values.MarginPadding16,
+					Right:  values.MarginPadding16,
+				},
+			}.Layout(gtx, func(gtx C) D {
+				return components.EndToEndRow(gtx, pg.version.Layout, pg.versionValue.Layout)
+			})
 		},
 		func(gtx C) D {
-			return components.EndToEndRow(gtx, pg.buildDate.Layout, pg.buildDateValue.Layout)
+			return components.Container{
+				Padding: layout.Inset{
+					Top:    values.MarginPadding20,
+					Bottom: values.MarginPadding20,
+					Left:   values.MarginPadding16,
+					Right:  values.MarginPadding16,
+				},
+			}.Layout(gtx, func(gtx C) D {
+				return components.EndToEndRow(gtx, pg.buildDate.Layout, pg.buildDateValue.Layout)
+			})
 		},
 		func(gtx C) D {
-			return components.EndToEndRow(gtx, pg.network.Layout, pg.networkValue.Layout)
+			return components.Container{
+				Padding: layout.Inset{
+					Top:    values.MarginPadding20,
+					Bottom: values.MarginPadding20,
+					Left:   values.MarginPadding16,
+					Right:  values.MarginPadding16,
+				},
+			}.Layout(gtx, func(gtx C) D {
+				return components.EndToEndRow(gtx, pg.network.Layout, pg.networkValue.Layout)
+			})
 		},
 		func(gtx C) D {
 			return decredmaterial.Clickable(gtx, pg.licenseRow, func(gtx C) D {
 				return layout.Flex{}.Layout(gtx,
-					layout.Rigid(pg.license.Layout),
+					layout.Rigid(func(gtx C) D {
+						return layout.Inset{
+							Top:    values.MarginPadding20,
+							Bottom: values.MarginPadding20,
+							Left:   values.MarginPadding16,
+							Right:  values.MarginPadding16,
+						}.Layout(gtx, pg.license.Layout)
+					}),
 					layout.Flexed(1, func(gtx C) D {
 						return layout.E.Layout(gtx, func(gtx C) D {
-							return pg.chevronRightIcon.Layout(gtx, values.MarginPadding20)
+							return layout.Inset{
+								Top:    values.MarginPadding20,
+								Bottom: values.MarginPadding20,
+								Left:   values.MarginPadding16,
+								Right:  values.MarginPadding16,
+							}.Layout(gtx, func(gtx C) D {
+								return pg.chevronRightIcon.Layout(gtx, values.MarginPadding20)
+							})
 						})
 					}),
 				)
@@ -150,16 +154,7 @@ func (pg *AboutPage) Layout(gtx layout.Context) layout.Dimensions {
 	return pg.container.Layout(gtx, len(w), func(gtx C, i int) D {
 		return layout.Inset{}.Layout(gtx, func(gtx C) D {
 			return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-				layout.Rigid(func(gtx C) D {
-					return components.Container{
-						Padding: layout.Inset{
-							Top:    values.MarginPadding20,
-							Bottom: values.MarginPadding20,
-							Left:   values.MarginPadding16,
-							Right:  values.MarginPadding16,
-						},
-					}.Layout(gtx, w[i])
-				}),
+				layout.Rigid(w[i]),
 				layout.Rigid(func(gtx C) D {
 					if i == len(w)-1 {
 						return layout.Dimensions{}
@@ -172,12 +167,11 @@ func (pg *AboutPage) Layout(gtx layout.Context) layout.Dimensions {
 		})
 	})
 }
-*/
 
 func (pg *AboutPage) Handle() {
-	/*if pg.licenseRow.Clicked() {
+	if pg.licenseRow.Clicked() {
 		pg.ChangeFragment(NewLicensePage(pg.Load))
-	}*/
+	}
 }
 
 func (pg *AboutPage) OnClose() {}
