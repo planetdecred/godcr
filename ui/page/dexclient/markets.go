2c66a456be3b6bc29aca92cec6b8eab45483aa3f
song50119 <>
2021-12-16 22:57:47 +0700

    Fix lint error

--- ui/page/dexclient/markets.go
+++ ui/page/dexclient/markets.go
@@ -304,7 +304,7 @@ func (pg *Page) HandleUserInteractions() {
 		dex.PendingFee == nil &&
 		pg.miniTradeFormWdg == nil {
 		mkt := pg.dexMarket(dex, marketIDSelected)
-		pg.miniTradeFormWdg = newMiniTradeFormWidget(pg.Load, dex, mkt)
+		pg.miniTradeFormWdg = newMiniTradeFormWidget(pg.Load, mkt)
 	}
 }
 
