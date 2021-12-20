a7647fe364558fc3208f0375740d52a1c5bd9e91
devchoplife <dritedurojaiye@gmail.com>
2021-12-20 19:46:16 +0100

    change slider background to Gray2

--- ui/decredmaterial/switch.go
+++ ui/decredmaterial/switch.go
@@ -174,7 +174,7 @@ func (s *SwitchButtonText) Layout(gtx layout.Context) layout.Dimensions {
 	m8 := unit.Dp(8)
 	m4 := unit.Dp(4)
 	card := s.t.Card()
-	card.Color = s.t.Color.Gray1
+	card.Color = s.t.Color.Gray2
 	card.Radius = Radius(8)
 	return card.Layout(gtx, func(gtx C) D {
 		return layout.UniformInset(unit.Dp(2)).Layout(gtx, func(gtx C) D {
