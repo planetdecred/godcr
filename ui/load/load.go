be0eb8d51a75eac19bf4339f63b1eb9d2209ab17
dreacot <kennedyizuegbu@yahoo.com>
2022-01-19 21:49:16 +0100

    add dcrlibwallet PR requirement

--- ui/load/load.go
+++ ui/load/load.go
@@ -183,31 +183,13 @@ func IconSet() Icons {
 		LogoDarkMode:             decredmaterial.NewImage(decredIcons["logo_darkmode"]),
 		TimerDarkMode:            decredmaterial.NewImage(decredIcons["timer_dm"]),
 		Rebroadcast:              decredmaterial.NewImage(decredIcons["rebroadcast"]),
-<<<<<<< HEAD
 
 		SettingsActiveIcon:       decredmaterial.NewImage(decredIcons["settings_active"]),
 		SettingsInactiveIcon:     decredmaterial.NewImage(decredIcons["settings_inactive"]),
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-		SettingsActiveIcon:       decredmaterial.NewImage(decredIcons["settings_active"]),
-		SettingsInactiveIcon:     decredmaterial.NewImage(decredIcons["settings_inactive"]),
-=======
-		ActivatedActiveIcon:     decredmaterial.NewImage(decredIcons["activated_active"]),
-		ActivatedInactiveIcon:   decredmaterial.NewImage(decredIcons["activated_inactive"]),
-		LockinActiveIcon:     decredmaterial.NewImage(decredIcons["lockin_active"]),
-		LockinInactiveIcon:   decredmaterial.NewImage(decredIcons["lockin_inactive"]),
->>>>>>> * add tab to governance page to switch between proposal and consensus pages
-=======
->>>>>>> code cleanup
 		ActivatedActiveIcon:      decredmaterial.NewImage(decredIcons["activated_active"]),
 		ActivatedInactiveIcon:    decredmaterial.NewImage(decredIcons["activated_inactive"]),
 		LockinActiveIcon:         decredmaterial.NewImage(decredIcons["lockin_active"]),
 		LockinInactiveIcon:       decredmaterial.NewImage(decredIcons["lockin_inactive"]),
-<<<<<<< HEAD
-=======
->>>>>>> code cleanup
->>>>>>> code cleanup
 
 		DexIcon:         decredmaterial.NewImage(decredIcons["dex_icon"]),
 		DexIconInactive: decredmaterial.NewImage(decredIcons["dex_icon_inactive"]),
