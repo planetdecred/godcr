4d1f8e0b7b566a1b43c4f27b6c6f34b1495083c0
devchoplife <dritedurojaiye@gmail.com>
2022-03-23 23:12:52 +0100

    Fix hover fill on add wallet menu

--- ui/page/wallets/wallet_page.go
+++ ui/page/wallets/wallet_page.go
@@ -1012,6 +1012,8 @@ func (pg *WalletPage) checkMixerSection(gtx layout.Context, listItem *walletList
 }
 
 func (pg *WalletPage) layoutAddWalletMenu(gtx layout.Context) layout.Dimensions {
+	width := unit.Value{U: unit.UnitDp, V: 55}
+	gtx.Constraints.Max.X = gtx.Px(width)
 	inset := layout.Inset{
 		Top:  unit.Dp(-100),
 		Left: unit.Dp(-130),
@@ -1021,9 +1023,21 @@ func (pg *WalletPage) layoutAddWalletMenu(gtx layout.Context) layout.Dimensions 
 		return pg.Theme.Shadow().Layout(gtx, func(gtx C) D {
 			return pg.optionsMenuCard.Layout(gtx, func(gtx C) D {
 				return (&layout.List{Axis: layout.Vertical}).Layout(gtx, len(pg.addWalletMenu), func(gtx C, i int) D {
-					return pg.addWalletMenu[i].button.Layout(gtx, func(gtx C) D {
-						return layout.UniformInset(unit.Dp(10)).Layout(gtx, pg.Theme.Body2(pg.addWalletMenu[i].text).Layout)
-					})
+					return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+						layout.Rigid(func(gtx C) D {
+							card := pg.Theme.Card()
+							card.Radius = decredmaterial.Radius(0)
+							return card.HoverableLayout(gtx, pg.addWalletMenu[i].button, func(gtx C) D {
+								return pg.addWalletMenu[i].button.Layout(gtx, func(gtx C) D {
+									return layout.UniformInset(values.MarginPadding10).Layout(gtx, func(gtx C) D {
+										gtx.Constraints.Min.X = gtx.Constraints.Max.X
+										return pg.Theme.Body2(pg.addWalletMenu[i].text).Layout(gtx)
+									})
+								})
+							})
+						}),
+					)
+
 				})
 			})
 		})
