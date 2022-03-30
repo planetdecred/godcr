95dbba78953f54b25c67fa9f6313851547e6de1f
monsa00 <66281245+monsa00@users.noreply.github.com>
2022-03-30 09:46:39 -0700

    current to active text update

--- ui/page/staking/overview.go
+++ ui/page/staking/overview.go
@@ -348,7 +348,7 @@ func (pg *Page) stakeLiveSection(gtx layout.Context) layout.Dimensions {
 			}),
 			layout.Rigid(func(gtx C) D {
 				if len(pg.liveTickets) == 0 {
-					noLiveStake := pg.Theme.Label(values.TextSize16, "No current tickets.")
+					noLiveStake := pg.Theme.Label(values.TextSize16, "No active tickets.")
 					noLiveStake.Color = pg.Theme.Color.GrayText3
 					return noLiveStake.Layout(gtx)
 				}
