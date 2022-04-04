27723ee08d4d15733ff5439013813a0d2e585873
AdimekweEbuka <adimekwe.ebuka@gmail.com>
2022-04-04 15:02:13 +0100

    code touch up for optimization

--- ui/page/overview/sync_details.go
+++ ui/page/overview/sync_details.go
@@ -126,7 +126,6 @@ func (pg *AppOverviewPage) progressBarRow(gtx C, inset layout.Inset) D {
 
 // progressStatusRow lays out the progress status when the wallet is syncing.
 func (pg *AppOverviewPage) progressStatusRow(gtx C, inset layout.Inset) D {
-	var timeLeftLabel decredmaterial.Label
 	timeLeft := pg.remainingSyncTime
 	progress := pg.syncProgress
 	rescanUpdate := pg.rescanUpdate
@@ -136,13 +135,12 @@ func (pg *AppOverviewPage) progressStatusRow(gtx C, inset layout.Inset) D {
 	}
 
 	percentageLabel := pg.Theme.Body1(fmt.Sprintf("%v%%", progress))
+	timeLeftLabel := fmt.Sprintf("%v left", timeLeft)
 	if progress == 0 {
-		timeLeftLabel = pg.Theme.Body1("connecting")
-	} else {
-		timeLeftLabel = pg.Theme.Body1(fmt.Sprintf("%v left", timeLeft))
+		timeLeftLabel = "connecting"
 	}
 	return inset.Layout(gtx, func(gtx C) D {
-		return components.EndToEndRow(gtx, percentageLabel.Layout, timeLeftLabel.Layout)
+		return components.EndToEndRow(gtx, percentageLabel.Layout, pg.Theme.Body1(timeLeftLabel).Layout)
 	})
 
 }
