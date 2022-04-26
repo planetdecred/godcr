481dae5104d23c66f0e78beb7c4f8f3278735352
Amos .E <anjeas4@gmail.com>
2022-04-26 12:17:15 +0100

    Refactor selectFirstWalletValidAccount to filter multiple accounts

--- ui/page/components/account_selector.go
+++ ui/page/components/account_selector.go
@@ -94,9 +94,9 @@ func (as *AccountSelector) Handle() {
 }
 
 // SelectFirstWalletValidAccount selects the first valid account from the
-// first wallet in the SortedWalletList, skipping any account whose ID is == filter.
+// first wallet in the SortedWalletList, skipping any account whose number is in filterAccounts slice.
 // If selectedWallet is not nil, the first account for the selectWallet is selected.
-func (as *AccountSelector) SelectFirstWalletValidAccount(selectedWallet *dcrlibwallet.Wallet, filter int32) error {
+func (as *AccountSelector) SelectFirstWalletValidAccount(selectedWallet *dcrlibwallet.Wallet, filterAccounts []int32) error {
 	if as.selectedAccount != nil && as.accountIsValid(as.selectedAccount) {
 		as.UpdateSelectedAccountBalance()
 		// no need to select account
@@ -111,10 +111,25 @@ func (as *AccountSelector) SelectFirstWalletValidAccount(selectedWallet *dcrlibw
 
 		accounts := accountsResult.Acc
 		for _, account := range accounts {
-			if as.accountIsValid(account) && account.Number != filter {
-				as.SetSelectedAccount(account)
-				as.callback(account)
-				return nil
+			if len(filterAccounts) > 0 {
+				accFound := false
+				for _, v := range filterAccounts {
+					if account.Number == v {
+						accFound = true
+					}
+				}
+
+				if as.accountIsValid(account) && !accFound {
+					as.SetSelectedAccount(account)
+					as.callback(account)
+					return nil
+				}
+			} else {
+				if as.accountIsValid(account) {
+					as.SetSelectedAccount(account)
+					as.callback(account)
+					return nil
+				}
 			}
 		}
 	}
@@ -127,10 +142,25 @@ func (as *AccountSelector) SelectFirstWalletValidAccount(selectedWallet *dcrlibw
 
 		accounts := accountsResult.Acc
 		for _, account := range accounts {
-			if as.accountIsValid(account) && account.Number != filter {
-				as.SetSelectedAccount(account)
-				as.callback(account)
-				return nil
+			if len(filterAccounts) > 0 {
+				accFound := false
+				for _, v := range filterAccounts {
+					if account.Number == v {
+						accFound = true
+					}
+				}
+
+				if as.accountIsValid(account) && !accFound {
+					as.SetSelectedAccount(account)
+					as.callback(account)
+					return nil
+				}
+			} else {
+				if as.accountIsValid(account) {
+					as.SetSelectedAccount(account)
+					as.callback(account)
+					return nil
+				}
 			}
 		}
 	}
